42347a01ebd923e84bbdb169dd1a2d4e5820a651
dhguevarah <dguevara@applaudostudios.com>
2022-02-25 11:45:03 -0600

    :sparkles: REV-96 Add Sentry

--- app/src/main/java/com/multimoney/multimoney/MultimoneyApplication.kt
+++ app/src/main/java/com/multimoney/multimoney/MultimoneyApplication.kt
@@ -1,7 +1,19 @@
 package com.multimoney.multimoney
 
 import android.app.Application
+import com.multimoney.multimoney.util.SentryHelper
 import dagger.hilt.android.HiltAndroidApp
 
+
 @HiltAndroidApp
-class MultimoneyApplication : Application()
+open class MultimoneyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initThirdPartySdks()
+    }
+
+    open fun initThirdPartySdks() {
+        SentryHelper.initSentry(this)
+    }
+}

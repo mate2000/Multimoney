4b1e3561a75c68793630f261526897c4261e0381
dhguevarah <dguevara@applaudostudios.com>
2022-03-24 09:19:45 -0600

    :lipstick: REV-436 Add typography, colors and molecules for buttons

--- app/src/main/java/com/multimoney/multimoney/presentation/theme/Type.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/theme/Type.kt
@@ -2,27 +2,107 @@ package com.multimoney.multimoney.presentation.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.multimoney.multimoney.R
 
-// Set of Material typography styles to start with
+val PoppinsFontFamily = FontFamily(
+    Font(R.font.poppins_regular)
+)
+
+// Set of Material typography styles
 val Typography = Typography(
+    h1 = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 96.sp,
+        letterSpacing = (-1.5).sp,
+        lineHeight = 112.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 60.sp,
+        letterSpacing = 0.5.sp,
+        lineHeight = 72.sp
+    ),
+    h3 = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 48.sp,
+        letterSpacing = 0.sp,
+        lineHeight = 56.sp
+    ),
+    h4 = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 34.sp,
+        letterSpacing = 0.25.sp,
+        lineHeight = 36.sp
+    ),
+    h5 = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 24.sp,
+        letterSpacing = 0.sp,
+        lineHeight = 24.sp
+    ),
+    h6 = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 20.sp,
+        letterSpacing = 0.15.sp,
+        lineHeight = 24.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 16.sp,
+        letterSpacing = 0.15.sp,
+        lineHeight = 24.sp
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 14.sp,
+        letterSpacing = 0.1.sp,
+        lineHeight = 24.sp
+    ),
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 16.sp,
+        letterSpacing = 0.5.sp,
+        lineHeight = 24.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 14.sp,
+        letterSpacing = 0.25.sp,
+        lineHeight = 20.sp
+    ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 14.sp,
+        letterSpacing = 1.25.sp,
+        lineHeight = 26.sp
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 12.sp,
+        letterSpacing = 0.4.sp,
+        lineHeight = 16.sp
+    ),
+    overline = TextStyle(
+        fontFamily = PoppinsFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 10.sp,
+        letterSpacing = 1.5.sp,
+        lineHeight = 16.sp
     )
-    */
 )
--- app/src/main/java/com/multimoney/multimoney/presentation/ui/test/TestScreen.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/test/TestScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.multimoney.domain.model.launch.LaunchConnection
+import com.multimoney.multimoney.presentation.uielement.common.CustomButton
 import com.multimoney.multimoney.presentation.util.UiEvent
 import com.onfido.android.sdk.capture.ExitCode
 import com.onfido.android.sdk.capture.Onfido
@@ -75,9 +76,10 @@ fun TestScreen(
 
 @Composable
 fun ChartButton(navigateToChart: () -> Unit) {
-    Button(onClick = navigateToChart, content = {
-        Text(text = "Chart")
-    })
+    CustomButton(
+        onClick = navigateToChart,
+        text = "Chart"
+    )
 }
 
 @Composable

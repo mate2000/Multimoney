86211f9a5955c41c52e75cd6f7403ae6f6ace299
rrubio01 <102053207+rrubio01@users.noreply.github.com>
2022-04-05 14:00:35 -0600

    ✨ REV-245 Add getStepContent

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/onboarding/OnBoardingViewModel.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/onboarding/OnBoardingViewModel.kt
@@ -1,6 +1,40 @@
 package com.multimoney.multimoney.presentation.ui.onboarding
 
+import com.multimoney.multimoney.R
 import com.multimoney.multimoney.presentation.base.BaseViewModel
 import javax.inject.Inject
 
-class OnBoardingViewModel @Inject constructor() : BaseViewModel()
+class OnBoardingViewModel @Inject constructor() : BaseViewModel() {
+
+    fun getStepContent(step: Int): List<Int> = when (step) {
+        STEP_ONE -> {
+            listOf(
+                R.string.onboarding_step_one_title,
+                R.string.onboarding_step_one_sub_title,
+                R.drawable.ic_onboarding_step_one
+            )
+        }
+        STEP_TWO -> {
+            listOf(
+                R.string.onboarding_step_two_title,
+                R.string.onboarding_step_two_sub_title,
+                R.drawable.ic_onboarding_step_two
+            )
+        }
+        else -> {
+            listOf(
+                R.string.onboarding_step_three_title,
+                R.string.onboarding_step_three_sub_title,
+                R.drawable.ic_onboarding_step_three
+            )
+        }
+    }
+
+    companion object {
+        const val STEP_ONE = 1
+        const val STEP_TWO = 2
+        const val STEP_TITLE = 0
+        const val STEP_SUBTITLE = 1
+        const val STEP_ICON = 2
+    }
+}

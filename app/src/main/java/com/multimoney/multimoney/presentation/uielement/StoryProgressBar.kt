87f966dddf9f9379e0b28678df59ef45282b45cb
rrubio01 <102053207+rrubio01@users.noreply.github.com>
2022-04-05 13:59:09 -0600

    💄 REV-245 Update StoryProgressBar

--- app/src/main/java/com/multimoney/multimoney/presentation/uielement/StoryProgressBar.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/uielement/StoryProgressBar.kt
@@ -4,13 +4,7 @@ import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -22,7 +16,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun CustomStoryProgressBar(
+fun StoryProgressBar(
     steps: Int,
     currentStep: Int,
     paused: Boolean,
@@ -33,21 +27,15 @@ fun CustomStoryProgressBar(
 ) {
     val percent = remember { Animatable(0f) }
     LaunchedEffect(paused, currentStep) {
-        if (paused) percent.stop()
-        else {
-            if (!paused) {
-                percent.snapTo(0f)
-            }
-            percent.animateTo(
-                targetValue = 1f,
-                animationSpec = tween(
-                    durationMillis = (10000 * (1f - percent.value)).toInt(),
-                    easing = LinearEasing
-                )
+        percent.snapTo(0f)
+        percent.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(
+                durationMillis = (10000 * (1f - percent.value)).toInt(),
+                easing = LinearEasing
             )
-            percent.snapTo(0f)
-            onFinished()
-        }
+        )
+        onFinished()
     }
     Row(verticalAlignment = Alignment.CenterVertically, modifier = modifier) {
         for (index in 1..steps) {

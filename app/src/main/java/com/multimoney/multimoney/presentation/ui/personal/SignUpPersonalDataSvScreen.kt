d04e6b3478fd7b923eb42e7173ecc27a6b9550f6
rrubio01 <102053207+rrubio01@users.noreply.github.com>
2022-04-21 15:19:30 -0600

    ✨ REV-391 Add SignUpPersonalDataSvScreen

---
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/personal/SignUpPersonalDataSvScreen.kt
@@ -0,0 +1,117 @@
+package com.multimoney.multimoney.presentation.ui.personal
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.multimoney.multimoney.R
+import com.multimoney.multimoney.presentation.uielement.CustomOutlinedTextField
+
+@Composable
+@Preview
+fun SignUpPersonalDataSvScreen(viewModel: SignUpPersonalDataViewModel = hiltViewModel()) {
+    val focusManager = LocalFocusManager.current
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(top = 16.dp)
+    ) {
+        CustomOutlinedTextField(
+            value = viewModel.personalDocumentValue,
+            placeHolder = stringResource(id = R.string.sing_up_sv_id_hint),
+            onValueChange = { if (it.length <= 9) viewModel.personalDocumentValue = it },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(onNext = {
+                focusManager.moveFocus(FocusDirection.Down)
+            }),
+            labelText = stringResource(id = R.string.sign_up_document_sv),
+            leadingIcon = R.drawable.ic_identification,
+            modifier = Modifier
+                .padding(top = 44.dp),
+            isRequired = true,
+            isRequiredMessage = stringResource(id = R.string.sign_up_dui_required),
+            isError = viewModel.personalIDError.first,
+            customTransformation = formatDui()
+        )
+        CustomOutlinedTextField(
+            placeHolder = stringResource(id = R.string.sing_up_name_hint),
+            value = viewModel.nameValue,
+            onValueChange = { viewModel.nameValue = it },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(onNext = {
+                focusManager.moveFocus(FocusDirection.Down)
+            }),
+            labelText = stringResource(id = R.string.sign_up_names),
+            modifier = Modifier.padding(top = 44.dp),
+            isRequired = true,
+            isRequiredMessage = stringResource(id = R.string.sign_up_name_error),
+            isError = viewModel.nameError.first,
+        )
+
+        CustomOutlinedTextField(
+            placeHolder = stringResource(id = R.string.sing_up_lastname_hint),
+            value = viewModel.lastNameValue,
+            onValueChange = { viewModel.lastNameValue = it },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(onNext = {
+                focusManager.moveFocus(FocusDirection.Down)
+            }),
+            labelText = stringResource(id = R.string.sign_up_lastname),
+            modifier = Modifier
+                .padding(top = 44.dp),
+            isRequired = true,
+            isRequiredMessage = stringResource(id = R.string.sign_up_lastname_error),
+            isError = viewModel.lastNameError.first
+        )
+    }
+}
+
+fun formatDui(): VisualTransformation =
+    object : VisualTransformation {
+        override fun filter(text: AnnotatedString): TransformedText {
+            val offset = object : OffsetMapping {
+                override fun originalToTransformed(offset: Int): Int {
+                    if (offset <= 7) return offset
+                    if (offset <= 8) return offset + 1
+                    return 10
+                }
+
+                override fun transformedToOriginal(offset: Int): Int {
+                    if (offset <= 7) return offset
+                    if (offset <= 8) return offset - 1
+                    return 9
+                }
+            }
+            return if (text.text.length >= 8) {
+                val formattedText =
+                    text.substring(0..7) + "-" + text.substring(8 until text.text.length)
+                TransformedText(AnnotatedString(formattedText), offset)
+            } else {
+                TransformedText(AnnotatedString(text.text), offset)
+            }
+        }
+    }

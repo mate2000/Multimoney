1f0e9fffd7ec3b8b5308efcc38b79979a5ebe445
rrubio01 <102053207+rrubio01@users.noreply.github.com>
2022-04-28 11:01:04 -0600

    🔥 REV-391 Remove formatId

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/personal/SignUpPersonalDataCrScreen.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/personal/SignUpPersonalDataCrScreen.kt
@@ -11,19 +11,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.input.TransformedText
-import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.multimoney.multimoney.R
 import com.multimoney.multimoney.presentation.ui.login.signup.SignUpViewModel
+import com.multimoney.multimoney.presentation.ui.trasnformation.formatId
 import com.multimoney.multimoney.presentation.uielement.CustomOutlinedTextField
 import com.multimoney.multimoney.presentation.uielement.CustomRadioButton
 
@@ -34,7 +30,6 @@ fun SignUpPersonalDataCrScreen(
     viewModel: SignUpPersonalDataViewModel = hiltViewModel()
 ) {
     val focusManager = LocalFocusManager.current
-    val documents = stringArrayResource(id = R.array.sign_up_costa_rica_documents)
     val customRadioModifier = Modifier
         .wrapContentSize()
         .selectable(
@@ -48,31 +43,27 @@ fun SignUpPersonalDataCrScreen(
     ) {
         CustomRadioButton(
             modifier = customRadioModifier.clickable {
-                viewModel.crPersonalDocument = documents[0]
+                viewModel.crPersonalDocument = CrDocuments.IdDocument.document
             },
             radioModifier = Modifier.padding(0.dp),
-            text = documents[0],
-            selected = viewModel.crPersonalDocument == documents[0],
-            onOptionSelected = { viewModel.crPersonalDocument = documents[0] }
+            text = CrDocuments.IdDocument.document,
+            selected = viewModel.crPersonalDocument == CrDocuments.IdDocument.document,
+            onOptionSelected = { viewModel.crPersonalDocument = CrDocuments.IdDocument.document }
         )
         CustomRadioButton(
             modifier = customRadioModifier.clickable {
-                viewModel.crPersonalDocument = documents[1]
+                viewModel.crPersonalDocument = CrDocuments.Dimex.document
             },
             radioModifier = Modifier.padding(0.dp),
-            selected = viewModel.crPersonalDocument == documents[1],
-            text = documents[1],
-            onOptionSelected = { viewModel.crPersonalDocument = documents[1] }
+            selected = viewModel.crPersonalDocument == CrDocuments.Dimex.document,
+            text = CrDocuments.Dimex.document,
+            onOptionSelected = { viewModel.crPersonalDocument = CrDocuments.Dimex.document }
         )
         CustomOutlinedTextField(
             value = viewModel.personalDocumentValue,
-            placeHolder = stringResource(id = if (viewModel.crPersonalDocument == documents[0]) R.string.sign_up_cr_id_hint else R.string.sign_up_cr_dimex_hint),
+            placeHolder = stringResource(id = if (viewModel.crPersonalDocument == CrDocuments.IdDocument.document) R.string.sign_up_cr_id_hint else R.string.sign_up_cr_dimex_hint),
             onValueChange = { newString ->
-                if (viewModel.crPersonalDocument == documents[0] && newString.length <= ID_LENGTH) {
-                    viewModel.personalDocumentValue = newString.filter { it.isDigit() }
-                } else if (viewModel.crPersonalDocument == documents[1] && newString.length <= DIMEX_LENGTH) {
-                    viewModel.personalDocumentValue = newString.filter { it.isDigit() }
-                }
+                viewModel.crFilterDocument(newString)
                 sharedViewModel.isContinueEnabled = viewModel.validateFields()
             },
             keyboardOptions = KeyboardOptions(
@@ -88,44 +79,13 @@ fun SignUpPersonalDataCrScreen(
             isRequired = true,
             isRequiredMessage = stringResource(id = R.string.sign_up_id_required),
             isError = viewModel.personalIDError.first,
-            customTransformation = if (viewModel.crPersonalDocument == documents[0]) formatId() else null,
+            customTransformation = if (viewModel.crPersonalDocument == CrDocuments.IdDocument.document) formatId() else null,
             onDebounceValidation = {
                 viewModel.validId(
-                    if (viewModel.crPersonalDocument == documents[0]) ID_LENGTH else DIMEX_LENGTH,
+                    if (viewModel.crPersonalDocument == CrDocuments.IdDocument.document) Nationalities.CostaRicaId.documentSize else Nationalities.CostaRicaDimex.documentSize,
                     R.string.sign_up_id_not_valid
                 )
             }
         )
     }
 }
-const val ID_LENGTH = 9
-const val DIMEX_LENGTH = 12
-
-fun formatId(): VisualTransformation =
-    object : VisualTransformation {
-        override fun filter(text: AnnotatedString): TransformedText {
-            val offset = object : OffsetMapping {
-                override fun originalToTransformed(offset: Int): Int {
-                    if (offset <= 1) return offset
-                    if (offset <= 4) return offset + 1
-                    if (offset <= 15) return offset + 2
-                    return 11
-                }
-
-                override fun transformedToOriginal(offset: Int): Int {
-                    if (offset <= 0) return offset
-                    if (offset <= 3) return offset - 1
-                    if (offset <= 14) return offset - 2
-                    return 10
-                }
-            }
-            var formattedText = ""
-            val trimmed = if (text.text.length >= 15) text.text.substring(0..14) else text.text
-
-            for (i in trimmed.indices) {
-                formattedText += trimmed[i]
-                if (i == 0 || i == 4) formattedText += " "
-            }
-            return TransformedText(AnnotatedString(formattedText), offset)
-        }
-    }

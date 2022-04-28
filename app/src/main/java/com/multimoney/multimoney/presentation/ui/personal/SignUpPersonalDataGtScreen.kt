a0c0e9684577ef79a8e24567861bf4f3402ef262
rrubio01 <102053207+rrubio01@users.noreply.github.com>
2022-04-28 12:25:17 -0600

    🔥 REV-391 Remove unused imports.

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/personal/SignUpPersonalDataGtScreen.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/personal/SignUpPersonalDataGtScreen.kt
@@ -10,12 +10,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.input.TransformedText
-import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel

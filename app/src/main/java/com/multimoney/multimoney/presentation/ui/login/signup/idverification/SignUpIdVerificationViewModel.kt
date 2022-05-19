3f772dfa6b299d659db5d7582922982b01df508b
rrubio01 <102053207+rrubio01@users.noreply.github.com>
2022-05-19 09:43:24 -0600

    ✨ REV-506 Add SignUpIdVerificationScreen

---
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/login/signup/idverification/SignUpIdVerificationViewModel.kt
@@ -0,0 +1,9 @@
+package com.multimoney.multimoney.presentation.ui.login.signup.idverification
+
+import com.multimoney.multimoney.presentation.base.BaseViewModel
+import com.multimoney.multimoney.presentation.util.onfido.OnFidoHelper
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SignUpIdVerificationViewModel @Inject constructor():BaseViewModel()

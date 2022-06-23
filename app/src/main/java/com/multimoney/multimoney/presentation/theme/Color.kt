c335db08ad401f6e093588fc47583a1165133329
rrubio01 <102053207+rrubio01@users.noreply.github.com>
2022-06-23 15:14:08 -0600

    💄 REV-593 Update colors

--- app/src/main/java/com/multimoney/multimoney/presentation/theme/Color.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/theme/Color.kt
@@ -6,36 +6,55 @@ import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
 // Primary
-val Primary200 = Color(0xFFDBE7FF)
-val Primary300 = Color(0xFF97BBFF)
-val Primary400 = Color(0xFF538EFF)
-val Primary500 = Color(0xFF0F62FF)
-val Primary600 = Color(0xFF0046CA)
-val Primary700 = Color(0xFF002E86)
+val Primary200 = Color(0xFFDBFFDB)
+val Primary300 = Color(0xFF8DEA8D)
+val Primary400 = Color(0xFF49D249)
+val Primary500 = Color(0xFF21B621)
+val Primary600 = Color(0xFF1A851A)
+val Primary700 = Color(0xFF015701)
+val Primary800 = Color(0xFF003100)
 
 // Secondary
-val Secondary500 = Color(0xFF4C49CD)
+val Secondary200 = Color(0xFFD4FCEB)
+val Secondary300 = Color(0xFFA6F2D3)
+val Secondary400 = Color(0xFF61E2AD)
+val Secondary500 = Color(0xFF24C281)
+val Secondary600 = Color(0xFF168F5D)
+val Secondary700 = Color(0xFF0E593A)
+val Secondary800 = Color(0xFF053A25)
+
+// Semantic Informative
+val SemanticInformative200 = Color(0xFFE5F0FF)
+val SemanticInformative300 = Color(0xFFB3D1FF)
+val SemanticInformative400 = Color(0xFF70A9FF)
+val SemanticInformative500 = Color(0xFF0066FF)
+val SemanticInformative600 = Color(0xFF0056D6)
+val SemanticInformative700 = Color(0xFF003380)
+val SemanticInformative800 = Color(0xFF001433)
 
 // Semantic Negative
+val SemanticNegative400 = Color (0xFFF37C7C)
 val SemanticNegative500 = Color(0xFFE91616)
 
 // Semantic Positive
+val SemanticPositive200 = Color(0XFFECFAEA)
+val SemanticPositive300 = Color(0XFFC5F0C1)
+val SemanticPositive400 = Color(0XFF92E38C)
+val SemanticPositive500 = Color(0XFF3CCD32)
 val SemanticPositive600 = Color(0XFF32AC2A)
-val LinkGreen = Color(0xFF69CA59)
-
-// Complementary 3
-val Complementary3500 = Color(0xFFFFBE11)
+val SemanticPositive700 = Color(0XFF1E6719)
+val SemanticPositive800 = Color(0XFF0C290A)
 
 // GrayScale
 val DefaultWhite = Color(0xFFFFFFFF)
 val DefaultBlack = Color(0xFF000000)
-val GrayScale200 = Color(0xFFF2F2F2)
+val GrayScale200 = Color(0xFFf2f2f2)
 val GrayScale300 = Color(0xFFD9D9D9)
 val GrayScale400 = Color(0xFFB8B8B8)
-val GrayScale500 = Color(0xFF808080)
-val GrayScale600 = Color(0xFF6B6B6B)
-val GrayScale700 = Color(0xFF404040)
-val GrayScale800 = Color(0xFF1A1A1A)
+val GrayScale500 = Color(0xFF8B8B8B)
+val GrayScale600 = Color(0xFF5F5F5F)
+val GrayScale700 = Color(0xFF393939)
+val GrayScale800 = Color(0xFF080808)
 
 // WhiteTransparency
 val WhiteTransparency10 = Color(0xFFFFFFFF).copy(alpha = 0.1f)
@@ -48,14 +67,22 @@ val WhiteTransparency80 = Color(0xFFFFFFFF).copy(alpha = 0.8f)
 val WhiteTransparency90 = Color(0xFFFFFFFF).copy(alpha = 0.9f)
 
 // BlackTransparency
+val BlackTransparency5 = Color(0xFF000000).copy(alpha = 0.05f)
+val BlackTransparency10 = Color(0xFF000000).copy(alpha = 0.1f)
+val BlackTransparency12 = Color(0xFF000000).copy(alpha = 0.12f)
+val BlackTransparency16 = Color(0xFF000000).copy(alpha = 0.16f)
+val BlackTransparency20 = Color(0xFF000000).copy(alpha = 0.2f)
+val BlackTransparency30 = Color(0xFF000000).copy(alpha = 0.3f)
+val BlackTransparency40 = Color(0xFF000000).copy(alpha = 0.4f)
 val BlackTransparency50 = Color(0xFF000000).copy(alpha = 0.5f)
+val BlackTransparency60 = Color(0xFF000000).copy(alpha = 0.6f)
+val BlackTransparency70 = Color(0xFF000000).copy(alpha = 0.7f)
+val BlackTransparency80 = Color(0xFF000000).copy(alpha = 0.8f)
+val BlackTransparency90 = Color(0xFF000000).copy(alpha = 0.9f)
 
 // Dark Theme
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
-
-// Todo this probably will change when the team of design update the design system
-val PrimaryBlack = Color(0xFF080808)
 
 @Immutable
 data class MultimoneyColors(
@@ -68,33 +95,36 @@ data class MultimoneyColors(
     val textInformation: Color,
     val textSubhead: Color,
     val divider: Color,
-    val circularProgressIndicator: Color
+    val circularProgressIndicator: Color,
+    val timerColor: Color
 )
 
 val DarkColorPalette = MultimoneyColors(
     primary = Purple700,
     secondary = Purple500,
-    background = DefaultBlack,
-    backgroundSplash = PrimaryBlack,
+    background = GrayScale800,
+    backgroundSplash = GrayScale800,
     text = DefaultWhite,
-    textLink = Primary500,
-    textInformation = Primary500,
+    textLink = Primary400,
+    textInformation = SemanticInformative400,
     textSubhead = GrayScale600,
     divider = GrayScale400,
-    circularProgressIndicator = DefaultWhite
+    circularProgressIndicator = DefaultWhite,
+    timerColor = DefaultWhite
 )
 
 val LightColorPalette = MultimoneyColors(
     primary = Primary500,
     secondary = Secondary500,
-    background = PrimaryBlack,
-    backgroundSplash = PrimaryBlack,
+    background = GrayScale200,
+    backgroundSplash = GrayScale200,
     text = GrayScale800,
-    textLink = Primary500,
-    textInformation = Primary500,
+    textLink = Primary400,
+    textInformation = SemanticInformative400,
     textSubhead = GrayScale600,
     divider = GrayScale400,
-    circularProgressIndicator = Primary500
+    circularProgressIndicator = Primary500,
+    timerColor = DefaultBlack
 )
 
 val LocalMultimoneyColors = staticCompositionLocalOf {

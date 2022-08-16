0ac6858a734097a01389ed5c66bb90b4968c1865
rrubio01 <102053207+rrubio01@users.noreply.github.com>
2022-08-16 17:19:23 -0600

    ✨ REV-827 Implement  dynamic document format

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/login/signup/personaldata/SignUpPersonalDataCrScreen.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/login/signup/personaldata/SignUpPersonalDataCrScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -22,18 +21,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.multimoney.multimoney.R
-import com.multimoney.multimoney.R.array
 import com.multimoney.multimoney.presentation.theme.MultimoneyTheme
 import com.multimoney.multimoney.presentation.theme.Typography
 import com.multimoney.multimoney.presentation.ui.login.signup.SignUpViewModel
 import com.multimoney.multimoney.presentation.ui.login.signup.SignUpViewModel.UIEvent.OnSharedIdentificationValueChange
+import com.multimoney.multimoney.presentation.ui.login.signup.personaldata.SignUpPersonalDataViewModel.Companion.FORMAT_VALUE
 import com.multimoney.multimoney.presentation.ui.login.signup.personaldata.SignUpPersonalDataViewModel.UIEvent.OnIdentificationTypeValueChange
 import com.multimoney.multimoney.presentation.ui.login.signup.personaldata.SignUpPersonalDataViewModel.UIEvent.OnIdentificationValueChange
 import com.multimoney.multimoney.presentation.uielement.CustomDropdown
 import com.multimoney.multimoney.presentation.uielement.CustomImage
 import com.multimoney.multimoney.presentation.uielement.CustomOutlinedTextField
 import com.multimoney.multimoney.presentation.util.CrDocuments
-import com.multimoney.multimoney.presentation.util.transformation.formatId
+import com.multimoney.multimoney.presentation.util.transformation.MaskVisualTransformation
 
 @Composable
 @Preview
@@ -52,7 +51,7 @@ fun SignUpPersonalDataCrScreen(
                 .wrapContentSize(Alignment.TopStart)
                 .focusable(false)
                 .padding(top = 16.dp),
-            items = stringArrayResource(id = array.sign_up_personal_cr_documents).sorted(),
+            items = viewModel.uiState.documentList,
             onValueChange = {
                 viewModel.onUIEvent(OnIdentificationTypeValueChange(it))
             },
@@ -62,7 +61,10 @@ fun SignUpPersonalDataCrScreen(
         )
         CustomOutlinedTextField(
             value = viewModel.uiState.personalDocumentValue,
-            placeHolder = stringResource(id = if (viewModel.uiState.identificationValueType == CrDocuments.IdDocument.document) R.string.sign_up_personal_data_cr_id_hint else R.string.sign_up_personal_data_cr_dimex_hint),
+            placeHolder = if (viewModel.uiState.documentFormat != "") viewModel.uiState.documentFormat.replace(
+                viewModel.uiState.documentFormat.last(),
+                FORMAT_VALUE, false
+            ) else "",
             onValueChange = { document ->
                 viewModel.onUIEvent(OnIdentificationValueChange(document) {
                     sharedViewModel.onUIEvent(
@@ -83,7 +85,10 @@ fun SignUpPersonalDataCrScreen(
             isRequiredMessage = stringResource(id = R.string.sign_up_personal_data_id_required),
             isError = viewModel.uiState.personalIdError.first,
             errorMessage = stringResource(id = viewModel.uiState.personalIdError.second),
-            customTransformation = if (viewModel.uiState.identificationValueType == CrDocuments.IdDocument.document) formatId() else null,
+            customTransformation = if (viewModel.uiState.documentFormat != "") MaskVisualTransformation(
+                viewModel.uiState.documentFormat,
+                viewModel.uiState.documentFormat.last()
+            ) else null,
             onDebounceValidation = {
                 viewModel.onUIEvent(
                     SignUpPersonalDataViewModel.UIEvent.OnValidateDocument(
--- app/src/main/java/com/multimoney/multimoney/presentation/ui/login/signup/personaldata/SignUpPersonalDataGtScreen.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/login/signup/personaldata/SignUpPersonalDataGtScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -22,14 +21,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.multimoney.multimoney.R
-import com.multimoney.multimoney.R.array
 import com.multimoney.multimoney.presentation.ui.login.signup.SignUpViewModel
 import com.multimoney.multimoney.presentation.ui.login.signup.SignUpViewModel.UIEvent.OnSharedIdentificationValueChange
+import com.multimoney.multimoney.presentation.ui.login.signup.personaldata.SignUpPersonalDataViewModel.Companion.FORMAT_VALUE
 import com.multimoney.multimoney.presentation.ui.login.signup.personaldata.SignUpPersonalDataViewModel.UIEvent.OnIdentificationTypeValueChange
 import com.multimoney.multimoney.presentation.ui.login.signup.personaldata.SignUpPersonalDataViewModel.UIEvent.OnIdentificationValueChange
 import com.multimoney.multimoney.presentation.uielement.CustomDropdown
 import com.multimoney.multimoney.presentation.uielement.CustomOutlinedTextField
-import com.multimoney.multimoney.presentation.util.transformation.formatDpi
+import com.multimoney.multimoney.presentation.util.transformation.MaskVisualTransformation
 
 @Composable
 @Preview
@@ -46,7 +45,7 @@ fun SignUpPersonalDataGtScreen(
                 .wrapContentSize(Alignment.TopStart)
                 .focusable(false)
                 .padding(top = 16.dp),
-            items = stringArrayResource(id = array.sign_up_personal_gt_documents).sorted(),
+            items = viewModel.uiState.documentList,
             onValueChange = {
                 viewModel.onUIEvent(OnIdentificationTypeValueChange(it))
             },
@@ -56,7 +55,10 @@ fun SignUpPersonalDataGtScreen(
         )
         CustomOutlinedTextField(
             value = viewModel.uiState.personalDocumentValue,
-            placeHolder = stringResource(id = R.string.sign_up_personal_data_gt_id_hint),
+            placeHolder = if (viewModel.uiState.documentFormat != "") viewModel.uiState.documentFormat.replace(
+                viewModel.uiState.documentFormat.last(),
+                FORMAT_VALUE, false
+            ) else "",
             onValueChange = { text ->
                 viewModel.onUIEvent(OnIdentificationValueChange(text) {
                     sharedViewModel.onUIEvent(
@@ -78,7 +80,10 @@ fun SignUpPersonalDataGtScreen(
             isRequiredMessage = stringResource(id = R.string.sign_up_personal_data_gt_id_required),
             isError = viewModel.uiState.personalIdError.first,
             errorMessage = stringResource(id = viewModel.uiState.personalIdError.second),
-            customTransformation = formatDpi(),
+            customTransformation = if (viewModel.uiState.documentFormat != "") MaskVisualTransformation(
+                viewModel.uiState.documentFormat,
+                viewModel.uiState.documentFormat.last()
+            ) else null,
             onDebounceValidation = {
                 viewModel.onUIEvent(SignUpPersonalDataViewModel.UIEvent.OnValidateDocument())
             }

837184eb4af3044e9b7c76f1ea88ecfcc8d754a9
Diego Sanchez <diego.sanchez.montes@gmail.com>
2022-08-30 20:16:43 -0600

    :bug: REV-1182 fix bug that shows enable the continue button in monthlyincome screen

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/credit/montlyincome/MonthlyIncomeScreen.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/credit/montlyincome/MonthlyIncomeScreen.kt
@@ -29,7 +29,6 @@ import com.multimoney.multimoney.R.string
 import com.multimoney.multimoney.presentation.theme.MultimoneyTheme
 import com.multimoney.multimoney.presentation.theme.Typography
 import com.multimoney.multimoney.presentation.ui.credit.CreditViewModel
-import com.multimoney.multimoney.presentation.ui.credit.montlyincome.MonthlyIncomeViewModel.BaseEvent.OnFormCompleted
 import com.multimoney.multimoney.presentation.ui.credit.montlyincome.MonthlyIncomeViewModel.UIEvent.OnProfessionValueChange
 import com.multimoney.multimoney.presentation.uielement.CustomDropdown
 import com.multimoney.multimoney.presentation.uielement.CustomOutlinedTextField
@@ -44,6 +43,17 @@ fun MonthlyIncomeScreen(
     val focusManager = LocalFocusManager.current
 
     LaunchedEffect(true) {
+        viewModel.baseEvent.collect { event ->
+            when (event) {
+                is MonthlyIncomeViewModel.BaseEvent.OnFormCompleted -> {
+                    sharedViewModel.onUIEvent(CreditViewModel.UIEvent.OnContinueEnable(event.isFormCompleted))
+                }
+            }
+        }
+    }
+
+    LaunchedEffect(true) {
+        viewModel.onUIEvent(MonthlyIncomeViewModel.UIEvent.OnValidForm)
         sharedViewModel.onUIEvent(CreditViewModel.UIEvent.OnSetNavigation(nextAction = {
             viewModel.onUIEvent(MonthlyIncomeViewModel.UIEvent.OnNextActionClick {
                 sharedViewModel.onUIEvent(
@@ -51,14 +61,6 @@ fun MonthlyIncomeScreen(
                 )
             })
         }, nextStep = CreditStep.Three.id, previousStep = CreditStep.One.id))
-        viewModel.onUIEvent(MonthlyIncomeViewModel.UIEvent.OnValidForm)
-        viewModel.baseEvent.collect { event ->
-            when (event) {
-                is OnFormCompleted -> {
-                    sharedViewModel.onUIEvent(CreditViewModel.UIEvent.OnContinueEnable(event.isFormCompleted))
-                }
-            }
-        }
     }
 
     Column(

39281a867da536db524273879b557acaaebbb923
David Melara <dmelara@applaudostudios.com>
2022-10-12 09:15:19 -0600

    ✨ REV-1817 Create bank screen viewmodel

---
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/credit/creditbank/CreditBankViewModel.kt
@@ -0,0 +1,9 @@
+package com.multimoney.multimoney.presentation.ui.credit.creditbank
+
+import com.multimoney.multimoney.presentation.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CreditBankViewModel @Inject constructor() : BaseViewModel(true) {
+}

9002e230f67aa3607b89c3f89ffa9132f42a98f2
rrubio01 <102053207+rrubio01@users.noreply.github.com>
2022-10-12 17:53:45 -0600

    ✨ REV-1186 Add CustomRoundedLinearProgress

---
+++ app/src/main/java/com/multimoney/multimoney/presentation/uielement/CustomRoundedLinearProgress.kt
@@ -0,0 +1,28 @@
+package com.multimoney.multimoney.presentation.uielement
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.LinearProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import com.multimoney.multimoney.presentation.theme.DefaultWhite
+import com.multimoney.multimoney.presentation.theme.SemanticPositive800
+
+@Composable
+fun CustomRoundedLinearProgress(progress: Float, modifier: Modifier) {
+    Box(
+        modifier = Modifier
+            .clip(RoundedCornerShape(15.dp))
+            .wrapContentHeight()
+            .fillMaxWidth()
+    ) {
+        LinearProgressIndicator(modifier = modifier,
+            progress = progress,
+            backgroundColor = SemanticPositive800,
+            color = DefaultWhite)
+    }
+}

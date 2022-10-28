72192fb727725e34f326eea0bd945743b1acebd3
David Melara <dmelara@applaudostudios.com>
2022-10-28 02:03:28 -0600

    ✨ REV-958 Get nav parameters for location

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/payment/location/LocationDetailsScreenViewModel.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/payment/location/LocationDetailsScreenViewModel.kt
@@ -3,16 +3,44 @@ package com.multimoney.multimoney.presentation.ui.payment.location
 
 import android.content.Context
 import androidx.compose.ui.focus.FocusManager
+import androidx.lifecycle.SavedStateHandle
 import com.multimoney.multimoney.presentation.base.BaseViewModel
 import com.multimoney.multimoney.presentation.navigation.Screen
+import com.multimoney.multimoney.presentation.navigation.navgraph.LOCATION_MAPS_ADDRESS
+import com.multimoney.multimoney.presentation.navigation.navgraph.LOCATION_MAPS_LATITUDE
+import com.multimoney.multimoney.presentation.navigation.navgraph.LOCATION_MAPS_LONGITUDE
+import com.multimoney.multimoney.presentation.navigation.navgraph.LOCATION_MAPS_NAME
+import com.multimoney.multimoney.presentation.navigation.navgraph.LOCATION_MAPS_OPENING_TIME
+import com.multimoney.multimoney.presentation.navigation.navgraph.LOCATION_MAPS_PAYMENT_AMOUNT
+import com.multimoney.multimoney.presentation.navigation.navgraph.LOCATION_MAPS_PAYMENT_ID
 import com.multimoney.multimoney.presentation.util.openMapsLink
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class LocationDetailsViewModel @Inject constructor(
-
+    savedStateHandle: SavedStateHandle
 ) : BaseViewModel(true) {
+
+
+    // Stateless
+    var locationName: String = ""
+    var locationAddress: String = ""
+    var locationOpeningTime: String = ""
+    var paymentAmount: String = ""
+    var paymentId: String = ""
+    var latitude: String = ""
+    var longitude: String = ""
+
+    init {
+        locationName = savedStateHandle[LOCATION_MAPS_NAME] ?: "1"
+        locationAddress = savedStateHandle[LOCATION_MAPS_ADDRESS] ?: "2"
+        locationOpeningTime = savedStateHandle[LOCATION_MAPS_OPENING_TIME] ?: "3"
+        paymentAmount = savedStateHandle[LOCATION_MAPS_PAYMENT_AMOUNT] ?: "4"
+        paymentId = savedStateHandle[LOCATION_MAPS_PAYMENT_ID] ?: "5"
+        latitude = savedStateHandle[LOCATION_MAPS_LATITUDE] ?: "6"
+        longitude = savedStateHandle[LOCATION_MAPS_LONGITUDE] ?: "7"
+    }
 
     private fun onCloseClick(focusManager: FocusManager) {
         focusManager.clearFocus()

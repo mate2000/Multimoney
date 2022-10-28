7290148e06f26fe049851c065edb964e5282a164
promero1207 <promero@applaudostudios.com>
2022-10-28 15:27:23 -0600

    no message

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/home/product/credit/CreditCardVariant.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/home/product/credit/CreditCardVariant.kt
@@ -2,22 +2,10 @@ package com.multimoney.multimoney.presentation.ui.home.product.credit
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Chip
-import androidx.compose.material.ChipDefaults
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.TrendingUp
@@ -43,22 +31,12 @@ import com.multimoney.multimoney.presentation.ui.home.product.ProductViewModel.U
 import com.multimoney.multimoney.presentation.ui.home.product.ProductViewModel.UIEvent.OnProgressCalculation
 import com.multimoney.multimoney.presentation.ui.home.product.credit.CreditApprovedOrStartedStatus.CreditStatusApproved
 import com.multimoney.multimoney.presentation.ui.home.product.credit.CreditApprovedOrStartedStatus.CreditStatusProcessStarted
-import com.multimoney.multimoney.presentation.ui.home.product.credit.CreditProcessStarted.CreditAcceptContractRefuseFirstTime
-import com.multimoney.multimoney.presentation.ui.home.product.credit.CreditProcessStarted.CreditAcceptContractRefuseSecondTime
-import com.multimoney.multimoney.presentation.ui.home.product.credit.CreditProcessStarted.CreditProcessCreateAccountFailure
-import com.multimoney.multimoney.presentation.ui.home.product.credit.CreditProcessStarted.CreditProcessMissingSignature
-import com.multimoney.multimoney.presentation.ui.home.product.credit.CreditProcessStarted.CreditProcessOnFidoIncomplete
-import com.multimoney.multimoney.presentation.ui.home.product.credit.CreditProcessStarted.CreditProcessSignatureRefuseFirstTime
-import com.multimoney.multimoney.presentation.ui.home.product.credit.CreditProcessStarted.CreditProcessSignatureRefuseSecondTime
-import com.multimoney.multimoney.presentation.ui.home.product.credit.CreditProcessStarted.CreditStartProcessIncomplete
+import com.multimoney.multimoney.presentation.ui.home.product.credit.CreditProcessStarted.*
 import com.multimoney.multimoney.presentation.uielement.CustomImage
 import com.multimoney.multimoney.presentation.uielement.CustomInformativeChip
 import com.multimoney.multimoney.presentation.uielement.CustomRoundedLinearProgress
-<<<<<<< HEAD
-import com.multimoney.multimoney.presentation.util.getCurrencySymbol
-=======
 import com.multimoney.multimoney.presentation.util.getCardDateFormat
->>>>>>> develop
+import com.multimoney.multimoney.presentation.util.getCurrency
 
 /**
  * Composable function to show the option to active accountsmart product
@@ -128,7 +106,7 @@ fun CardSmartProduct(
             color = MultimoneyTheme.colors.text
         )
         Text(
-            text = "${brandId.getCurrencySymbol()}$profitTotal",
+            text = "${brandId.getCurrency()}$profitTotal",
             modifier = Modifier.padding(bottom = 10.dp),
             style = Typography.h4.copy(fontWeight = FontWeight.SemiBold),
             color = MultimoneyTheme.colors.text
@@ -162,7 +140,7 @@ fun CardSmartProduct(
                         tint = MultimoneyTheme.colors.smartCardPlus
                     )
                     Text(
-                        text = "${brandId.getCurrencySymbol()}$profitMonthly ${stringResource(id = R.string.smart_card_monthly_profit_label)} $currentMonth",
+                        text = "${brandId.getCurrency()}$profitMonthly ${stringResource(id = R.string.smart_card_monthly_profit_label)} $currentMonth",
                         modifier = Modifier.padding(start = 4.dp),
                         style = Typography.body2.copy(fontWeight = FontWeight.SemiBold),
                         color = MultimoneyTheme.colors.text
@@ -628,20 +606,12 @@ fun OngoingCredit(
                                 .size(10.dp)
                                 .clip(CircleShape)
                                 .background(
-<<<<<<< HEAD
-                                    if ((viewModel.balanceCredit?.getFirstSummary()?.daysExpired
-                                            ?: 0) > 0
-                                    ) MultimoneyTheme.colors.dotIndicatorExpired else MultimoneyTheme.colors.tipActionColor
-                                )
-                                .padding(top = 2.dp, start = 3.dp)
-=======
                                     if ((
                                         viewModel.balanceCredit?.getFirstSummary()?.daysExpired
                                             ?: 0
                                         ) > 0
                                     ) MultimoneyTheme.colors.dotIndicatorExpired else MultimoneyTheme.colors.tipActionColor
                                 )
->>>>>>> develop
                         )
                     },
                     onClick = {

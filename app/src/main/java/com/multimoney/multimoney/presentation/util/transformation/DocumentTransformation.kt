0794f18578fc557032da10b08d535ed80f9ba5d7
mmilenkoff <mmilenkoff@applaudostudios.com>
2022-11-01 08:38:58 -0300

    🐛 Fix visual transformation

--- app/src/main/java/com/multimoney/multimoney/presentation/util/transformation/DocumentTransformation.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/util/transformation/DocumentTransformation.kt
@@ -97,17 +97,17 @@ fun formatBusinessIdentification(): VisualTransformation =
         override fun filter(text: AnnotatedString): TransformedText {
             val offset = object : OffsetMapping {
                 override fun originalToTransformed(offset: Int): Int {
-                    if (offset <= 1) return offset
-                    if (offset <= 4) return offset + 1
-                    if (offset <= 10) return offset + 2
-                    return 10
+                    if (offset <= 2) return offset
+                    if (offset <= 5) return offset + 1
+                    if (offset <= 11) return offset + 2
+                    return 11
                 }
 
                 override fun transformedToOriginal(offset: Int): Int {
-                    if (offset <= 0) return offset
-                    if (offset <= 3) return offset - 1
+                    if (offset <= 1) return offset
+                    if (offset <= 4) return offset - 1
                     if (offset <= 9) return offset - 2
-                    return 9
+                    return 10
                 }
             }
 

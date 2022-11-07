97fd98d5df10d8fe886c16af4f6c92c813a7bfe1
mmilenkoff <mmilenkoff@applaudostudios.com>
2022-11-07 12:55:08 -0300

    :bug: fix imports

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/home/product/ProductViewModel.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/home/product/ProductViewModel.kt
@@ -215,32 +215,7 @@ class ProductViewModel @Inject constructor(
     }
 
     private fun onNavigateToPaymentScreen() {
-        val creditSummary = balanceCredit?.balanceCredit?.first()?.summary
-        val infoCredit = uiState.userStatus?.infoCredit
-        val route = if ((
-            creditSummary?.size
-                ?: 0
-            ) > 1 && validateQuotas(creditSummary) && uiState.idBrand.toInt() == Brand.CostaRica.id
-        ) {
-            "${Screen.PaymentFeeScreen.baseRoute}/$email/${uiState.idBrand}/${infoCredit?.idClient}/${infoCredit?.idLoanClient}/${
-            encodeData(
-                creditSummary
-            )
-            }/$identification/$userName"
-        } else if (uiState.idBrand.toInt() == Brand.CostaRica.id) {
-            "${Screen.PaymentAccountScreen.baseRoute}/$email/${uiState.idBrand}/${infoCredit?.idClient}/${infoCredit?.idLoanClient}/${
-            encodeData(
-                listOf(creditSummary?.firstOrNull { (it.currentBalance ?: ZERO) > ZERO })
-            )
-            }/$identification/$userName"
-        } else {
-            "${Screen.PaymentOptionsScreen.baseRoute}/${uiState.idBrand}/${balanceCredit?.getFirstCredit()?.creditNumber}/${
-            encodeData(
-                configurationVersion?.configuration?.credit?.paymentMethod?.filter { it?.active == true }
-            )
-            }/${encodeData(configurationVersion?.configuration?.credit?.transferAccount)}"
-        }
-        navigateTo(route)
+        navigateTo("${Screen.SmartScreen.baseRoute}/${userName}/${uiState.idBrand}/${pkUser}")
     }
 
     private fun validateQuotas(summaryList: List<Summary>?): Boolean {

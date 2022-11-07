a8d1bb5eaf07750078018e041771bd5c5d3d56be
Antonio Guerra <anapoleon@applaudostudios.com>
2022-11-07 18:42:24 -0400

    ✨ REV-1769 Add api calls and mapping data in in origination second step screen

--- data/src/main/java/com/multimoney/data/networking/GraphqlApi.kt
+++ data/src/main/java/com/multimoney/data/networking/GraphqlApi.kt
@@ -28,10 +28,12 @@ import com.multimoney.data.networking.graphql.apollomodel.HomeCantonQuery
 import com.multimoney.data.networking.graphql.apollomodel.HomeDistrictQuery
 import com.multimoney.data.networking.graphql.apollomodel.HomeProvinceQuery
 import com.multimoney.data.networking.graphql.apollomodel.NationalityQuery
+import com.multimoney.data.networking.graphql.apollomodel.OcupationsQuery
 import com.multimoney.data.networking.graphql.apollomodel.OnfidoIntialProcessMutation
 import com.multimoney.data.networking.graphql.apollomodel.PaymentAmountQuery
 import com.multimoney.data.networking.graphql.apollomodel.ProccessPaymentListMutation
 import com.multimoney.data.networking.graphql.apollomodel.ProfessionSmartQuery
+import com.multimoney.data.networking.graphql.apollomodel.ProfessionsQuery
 import com.multimoney.data.networking.graphql.apollomodel.SaveCreditApplicationMutation
 import com.multimoney.data.networking.graphql.apollomodel.SaveCreditFlowInputMutation
 import com.multimoney.data.networking.graphql.apollomodel.ScreenConfigQuery
@@ -234,6 +236,24 @@ class GraphqlApi @Inject constructor(
                 currentStep
             )
         ).fetchPolicy(FetchPolicy.NetworkOnly)
+
+    fun queryProfession(
+        pkUser: Int,
+        user: String,
+        idBrand: Int,
+        idUserRequest: Int
+    ): ApolloCall<ProfessionsQuery.Data> =
+        apolloAuthorizedClient.query(ProfessionsQuery(pkUser, user, idBrand, idUserRequest))
+            .fetchPolicy(FetchPolicy.NetworkOnly)
+
+    fun queryOccupation(
+        pkUser: Int,
+        user: String,
+        idBrand: Int,
+        idUserRequest: Int
+    ): ApolloCall<OcupationsQuery.Data> =
+        apolloAuthorizedClient.query(OcupationsQuery(pkUser, user, idBrand, idUserRequest))
+            .fetchPolicy(FetchPolicy.NetworkOnly)
 
     fun mutationTermsAndConditions(
         user: String,

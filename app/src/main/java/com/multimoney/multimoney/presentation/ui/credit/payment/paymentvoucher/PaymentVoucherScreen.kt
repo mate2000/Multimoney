d7b577d1f385e293d2140b2a1483cedaf6c33a4c
Jose Arenas <arenas782@gmail.com>
2022-11-15 11:20:12 -0400

    fixing imports

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/credit/payment/paymentvoucher/PaymentVoucherScreen.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/credit/payment/paymentvoucher/PaymentVoucherScreen.kt
@@ -1,15 +1,15 @@
 package com.multimoney.multimoney.presentation.ui.credit.payment.paymentvoucher
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -29,7 +29,6 @@ import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview

63cd0399e35dda3c7ad3dbb36a4a3e549ce436ea
mmilenkoff <mmilenkoff@applaudostudios.com>
2022-11-17 09:29:45 -0300

    :sparkles: add dialog for request error

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/smart/origination/sourceincome/options/formalsalariedsv/FormalSalariedSvScreen.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/smart/origination/sourceincome/options/formalsalariedsv/FormalSalariedSvScreen.kt
@@ -30,10 +30,12 @@ import com.multimoney.multimoney.presentation.ui.smart.SmartViewModel
 import com.multimoney.multimoney.presentation.ui.smart.SmartViewModel.UIEvent.OnCallMutationUpdateGlobalRequestUseCase
 import com.multimoney.multimoney.presentation.ui.smart.SmartViewModel.UIEvent.OnContinueEnable
 import com.multimoney.multimoney.presentation.ui.smart.SmartViewModel.UIEvent.OnContinueVisible
+import com.multimoney.multimoney.presentation.ui.smart.SmartViewModel.UIEvent.OnFailureWithDialog
 import com.multimoney.multimoney.presentation.ui.smart.SmartViewModel.UIEvent.OnSetNavigation
 import com.multimoney.multimoney.presentation.ui.smart.origination.sourceincome.SourceIncomeViewModel
 import com.multimoney.multimoney.presentation.ui.smart.origination.sourceincome.SourceIncomeViewModel.UIEvent.OnNavigateToSelectedSourceOfIncomeOption
 import com.multimoney.multimoney.presentation.ui.smart.origination.sourceincome.options.formalsalariedsv.FormalSalariedSvViewModel.BaseEvent.OnFormValidateCompleted
+import com.multimoney.multimoney.presentation.ui.smart.origination.sourceincome.options.formalsalariedsv.FormalSalariedSvViewModel.BaseEvent.OnRequestError
 import com.multimoney.multimoney.presentation.ui.smart.origination.sourceincome.options.formalsalariedsv.FormalSalariedSvViewModel.UIEvent.OnCompanyNameChange
 import com.multimoney.multimoney.presentation.ui.smart.origination.sourceincome.options.formalsalariedsv.FormalSalariedSvViewModel.UIEvent.OnDivisionThreeValueChange
 import com.multimoney.multimoney.presentation.ui.smart.origination.sourceincome.options.formalsalariedsv.FormalSalariedSvViewModel.UIEvent.OnDivisionTwoValueChange
@@ -79,10 +81,18 @@ fun FormalSalariedSvScreen(
                 previousStep = Three.id
             )
         )
+
         viewModel.baseEvent.collect { event ->
             when (event) {
                 is OnFormValidateCompleted -> sharedViewModel.onUIEvent(
                     OnContinueEnable(event.isFormValid)
+                )
+
+                is OnRequestError -> sharedViewModel.onUIEvent(
+                    OnFailureWithDialog(
+                        isLoading = false,
+                        openDialog = event.dialogParameters
+                    )
                 )
             }
         }
--- app/src/main/java/com/multimoney/multimoney/presentation/ui/smart/origination/sourceincome/options/formalsalariedsv/FormalSalariedSvViewModel.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/smart/origination/sourceincome/options/formalsalariedsv/FormalSalariedSvViewModel.kt
@@ -6,6 +6,8 @@ import androidx.compose.runtime.setValue
 import com.multimoney.domain.interaction.accountsmart.QueryAddressLevelThreeUseCase
 import com.multimoney.domain.interaction.accountsmart.QueryAddressLevelTwoUseCase
 import com.multimoney.domain.model.accountsmart.Address
+import com.multimoney.domain.model.util.onFailure
+import com.multimoney.domain.model.util.onLoading
 import com.multimoney.domain.model.util.onSuccess
 import com.multimoney.multimoney.R
 import com.multimoney.multimoney.presentation.base.BaseViewModel
@@ -19,6 +21,7 @@ import com.multimoney.multimoney.presentation.ui.smart.origination.sourceincome.
 import com.multimoney.multimoney.presentation.ui.smart.origination.sourceincome.options.formalsalariedsv.FormalSalariedSvViewModel.UIEvent.OnWorkingAddressChange
 import com.multimoney.multimoney.presentation.util.DECIMAL_REGEX
 import com.multimoney.multimoney.presentation.util.DESCRIPTION_MAX_LENGTH
+import com.multimoney.multimoney.presentation.util.catalog.DialogParameters
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.collectLatest
 import java.util.regex.Pattern
@@ -47,6 +50,19 @@ class FormalSalariedSvViewModel @Inject constructor(
                 it.onSuccess { addressList ->
                     uiState = uiState.copy(divisionTwoList = addressList?.addresses)
                 }
+                it.onLoading {
+                    uiState = uiState.copy(isLoading = true)
+                }
+                it.onFailure { error ->
+                    uiState = uiState.copy(
+                        dialogParameters = DialogParameters(
+                            description = error.getError() ?: "",
+                            isActive = mutableStateOf(true)
+                        ),
+                        isLoading = false
+                    )
+                    onRequestError()
+                }
             }
         }
     }
@@ -61,7 +77,23 @@ class FormalSalariedSvViewModel @Inject constructor(
                     idAddressLevelTwo = divTwo
                 ).collectLatest {
                     it.onSuccess { addressList ->
-                        uiState = uiState.copy(divisionThreeList = addressList?.addresses)
+                        uiState = uiState.copy(
+                            divisionThreeList = addressList?.addresses,
+                            isLoading = false
+                        )
+                    }
+                    it.onLoading {
+                        uiState = uiState.copy(isLoading = true)
+                    }
+                    it.onFailure { error ->
+                        uiState = uiState.copy(
+                            dialogParameters = DialogParameters(
+                                description = error.getError() ?: "",
+                                isActive = mutableStateOf(true)
+                            ),
+                            isLoading = false
+                        )
+                        onRequestError()
                     }
                 }
             }
@@ -116,6 +148,7 @@ class FormalSalariedSvViewModel @Inject constructor(
     }
 
     private fun onValidateForm() = emitBaseEvent(BaseEvent.OnFormValidateCompleted(isFormValid()))
+    private fun onRequestError() = emitBaseEvent(BaseEvent.OnRequestError(uiState.dialogParameters))
 
     fun isFormValid(): Boolean =
         uiState.profession.isNotBlank() &&
@@ -134,7 +167,9 @@ class FormalSalariedSvViewModel @Inject constructor(
         val divisionTwoList: List<Address?>? = listOf(),
         val divisionThreeList: List<Address?>? = listOf(),
         val divisionTwoSelected: Address? = null,
-        val divisionThreeSelected: Address? = null
+        val divisionThreeSelected: Address? = null,
+        val isLoading: Boolean = false,
+        val dialogParameters: DialogParameters = DialogParameters()
     )
 
     sealed class UIEvent {
@@ -167,6 +202,7 @@ class FormalSalariedSvViewModel @Inject constructor(
 
     sealed class BaseEvent {
         data class OnFormValidateCompleted(val isFormValid: Boolean) : BaseEvent()
+        data class OnRequestError(val dialogParameters: DialogParameters) : BaseEvent()
     }
 
     companion object {

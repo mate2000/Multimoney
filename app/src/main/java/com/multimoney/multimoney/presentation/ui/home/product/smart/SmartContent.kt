464a9a539c238aafe880bb9c9593146587267c28
Jose Arenas <arenas782@gmail.com>
2022-11-18 14:39:08 -0400

    separating smart card and adding logic according to bank account status instead of balances

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/home/product/smart/SmartContent.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/home/product/smart/SmartContent.kt
@@ -5,45 +5,51 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.multimoney.multimoney.presentation.ui.home.product.ProductViewModel
-import com.multimoney.multimoney.presentation.ui.home.product.ProductViewModel.UIEvent.OnNavigateToSmartOriginationFlow
-import com.multimoney.multimoney.presentation.ui.home.product.credit.uisections.CardOfferSmartProduct
-import com.multimoney.multimoney.presentation.ui.home.product.credit.uisections.CardSmartProduct
+import com.multimoney.multimoney.presentation.ui.home.product.smart.uisections.CardInactiveSmartProduct
+import com.multimoney.multimoney.presentation.ui.home.product.smart.uisections.CardSmartProduct
 import com.multimoney.multimoney.presentation.uielement.CustomProductBackground
 import com.multimoney.multimoney.presentation.uielement.ProductBackGroundType
 
 @Composable
 fun SmartContent(viewModel: ProductViewModel, currentPage: Int) {
-    viewModel.balanceCredit?.balanceAccountSmart?.let {
-        if (it.isNotEmpty()) {
-            CustomProductBackground(
-                modifier = Modifier.padding(horizontal = 16.dp),
-                type = ProductBackGroundType.Secondary
-            ) {
-                CardSmartProduct(
-                    currency = it[
-                        currentPage.minus(
-                            viewModel.balanceCredit?.balanceCredit?.size ?: 0
+    when (viewModel.uiState.userStatus?.infoBankAccount?.status){
+        1 -> {
+            viewModel.balanceCredit?.balanceAccountSmart?.let {
+                if (it.isNotEmpty()) {
+                    CustomProductBackground(
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        type = ProductBackGroundType.Secondary
+                    ) {
+                        CardSmartProduct(
+                            currency = it[
+                                    currentPage.minus(
+                                        viewModel.balanceCredit?.balanceCredit?.size ?: 0
+                                    )
+                            ]?.currencyCode ?: "",
+                            profitMonthly = it[
+                                    currentPage.minus(
+                                        viewModel.balanceCredit?.balanceCredit?.size ?: 0
+                                    )
+                            ]?.gainedInterest.toString(),
+                            profitTotal = it[
+                                    currentPage.minus(
+                                        viewModel.balanceCredit?.balanceCredit?.size ?: 0
+                                    )
+                            ]?.totalBalance.toString()
                         )
-                    ]?.currencyCode ?: "",
-                    profitMonthly = it[
-                        currentPage.minus(
-                            viewModel.balanceCredit?.balanceCredit?.size ?: 0
-                        )
-                    ]?.gainedInterest.toString(),
-                    profitTotal = it[
-                        currentPage.minus(
-                            viewModel.balanceCredit?.balanceCredit?.size ?: 0
-                        )
-                    ]?.totalBalance.toString()
-                )
+                    }
+                }
             }
-        } else {
-            CustomProductBackground(
-                modifier = Modifier.padding(horizontal = 16.dp),
-                type = ProductBackGroundType.Secondary
-            ) {
-                CardOfferSmartProduct() {
-                    viewModel.onUIEvent(OnNavigateToSmartOriginationFlow)
+        }
+        0 -> {
+            viewModel.uiState.userStatus?.infoBankAccount?.wording.let {
+                CustomProductBackground(
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    type = ProductBackGroundType.Secondary
+                ) {
+                    CardInactiveSmartProduct(it?.textOne.toString(),it?.textTwo.toString(),it?.cTA.toString()){
+                        //TODO add navigation according to status
+                    }
                 }
             }
         }

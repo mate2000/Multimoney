1a5d24b23fb0f2031285ccb600eb565d7c907e32
Antonio Guerra <anapoleon@applaudostudios.com>
2022-11-22 22:43:51 -0400

    ✨ REV-1196 REV-2334 Add Schedule Automatic Payment feature and fix some bugs

---
+++ app/src/main/java/com/multimoney/multimoney/presentation/uielement/CustomTextButton.kt
@@ -0,0 +1,61 @@
+package com.multimoney.multimoney.presentation.uielement
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.multimoney.multimoney.R
+import com.multimoney.multimoney.presentation.theme.MultimoneyTheme
+import com.multimoney.multimoney.presentation.theme.Typography
+
+/**
+ * CustomTextButton: This component is used to create text buttons*
+ * Parameters:
+ * @param textResource: Text to convert into button
+ * @param endIconResource: End Icon to complement button
+ * @param onClick: This will happen when user perform click on text button
+ */
+
+@Composable
+fun CustomTextButton(
+    textResource: Int = R.string.empty,
+    endIconResource: Int = 0,
+    onClick: () -> Unit = {}
+) {
+    val textColor = if (isSystemInDarkTheme()) {
+        MultimoneyTheme.colors.textLink
+    } else {
+        MultimoneyTheme.colors.textLink
+    }
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(id = textResource),
+            modifier = Modifier.clickable {
+                onClick()
+            },
+            style = Typography.button.copy(fontSize = 14.sp),
+            color = textColor
+        )
+        Spacer(modifier = Modifier.width(7.dp))
+        Image(
+            modifier = Modifier.size(20.dp),
+            painter = painterResource(id = endIconResource),
+            contentDescription = ""
+        )
+    }
+}
--- app/src/main/java/com/multimoney/multimoney/presentation/util/DateUtils.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/util/DateUtils.kt
@@ -2,8 +2,8 @@ package com.multimoney.multimoney.presentation.util
 
 import java.text.SimpleDateFormat
 import java.util.Calendar
-import java.util.Locale
 import java.util.Date
+import java.util.Locale
 
 fun getPickedDateAsString(year: Int, month: Int, day: Int, dateFormat: String): String {
     val calendar = Calendar.getInstance()
@@ -22,11 +22,11 @@ fun getFormatDateByString(date: String, formatOne: String, formatTwo: String): S
     }
 }
 
-fun getCardDateFormat(date: String?): String {
+fun getCardDateFormat(date: String?, format: SimpleDateFormat = BAR_DIVIDER_FORMAT): String {
     return if (date.isNullOrEmpty().not()) {
         val dateFormatted = SHORT_DATE_FORMAT.parse(date)
         dateFormatted?.let {
-            BAR_DIVIDER_FORMAT.format(dateFormatted)
+            format.format(dateFormatted)
         } ?: run {
             ""
         }
@@ -48,11 +48,11 @@ fun getDayFromString(date: String?, format: SimpleDateFormat): String {
     }
 }
 
-fun getCurrentDate(time : Date) : String{
+fun getCurrentDate(time: Date): String {
     return BAR_DIVIDER_FORMAT.format(time)
 }
 
-fun getCurrentTime(time : Date) : String{
+fun getCurrentTime(time: Date): String {
     return SHORT_TIME_FORMAT.format(time)
 }
 
@@ -64,4 +64,4 @@ val API_DATE_FORMAT = SimpleDateFormat(ISO_8601_API_FORMAT_PATTERN, Locale.getDe
 val SHORT_DATE_FORMAT = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
 val BAR_DIVIDER_FORMAT = SimpleDateFormat("dd | MM | yyyy", Locale.getDefault())
 val SHORT_TIME_FORMAT = SimpleDateFormat("hh:mm a", Locale.getDefault())
-
+val BAR_DIVIDER_FORMAT_YEAR_TWO_DIGITS = SimpleDateFormat("dd | MM | yy", Locale.getDefault())

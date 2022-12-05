cc37b01b9bb0240e1438a6cc7f93b1a00b5adbdd
Jeseap <jsalvador@applaudostudios.com>
2022-12-05 16:34:05 -0600

    🚧 Add continuevalidatingonfidoscreen file

---
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/smart/origination/onfidoscenarios/continuevalidatingonfido/ContinueValidatingOnfidoScreen.kt
@@ -0,0 +1,15 @@
+package com.multimoney.multimoney.presentation.ui.smart.origination.onfidoscenarios.continuevalidatingonfido
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import com.multimoney.multimoney.presentation.util.NavEvent
+
+@Composable
+fun ContinueValidatingOnfidoScreen(
+    onPopAndNavigate: (NavEvent.PopAndNavigate) -> Unit = {},
+   // viewModel: ContinueValidatingOnfidoViewModel = hiltViewModel()
+) {
+    LaunchedEffect(key1 = true) {
+        // TODO: add the viewModel.executeNavigation
+    }
+}

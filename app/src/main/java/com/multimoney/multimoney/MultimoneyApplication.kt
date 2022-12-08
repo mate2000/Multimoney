ddc414b8218b96ed8cc66b3d59ef187880924493
Dsanchez1992 <103954910+Dsanchez1992@users.noreply.github.com>
2022-12-08 17:04:45 -0600

    :wrench: REV-999 Add novo configuration

--- app/src/main/java/com/multimoney/multimoney/MultimoneyApplication.kt
+++ app/src/main/java/com/multimoney/multimoney/MultimoneyApplication.kt
@@ -1,9 +1,11 @@
 package com.multimoney.multimoney
 
 import android.app.Application
+import com.multimoney.multimoney.presentation.util.NfcHelper
 import com.multimoney.multimoney.util.AdjustHelper
 import com.multimoney.multimoney.util.CognitoHelper
 import com.multimoney.multimoney.util.SentryHelper
+import com.novopayment.sdk.vts.NovoVTS
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
@@ -19,6 +21,9 @@ open class MultimoneyApplication : Application() {
     @Inject
     lateinit var cognitoHelper: CognitoHelper
 
+    @Inject
+    lateinit var nfcHelper: NfcHelper
+
     override fun onCreate() {
         super.onCreate()
         initThirdPartySdks()
@@ -31,5 +36,8 @@ open class MultimoneyApplication : Application() {
         sentryHelper.initSentry()
         adjustHelper.initAdjust()
         cognitoHelper.initCognito()
+        if (nfcHelper.isNfcSupported()) {
+            NovoVTS.isInitialized()
+        }
     }
 }

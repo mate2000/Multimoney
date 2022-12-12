4f743828ef97d658599ca0c4d83b179a0f099ea5
rrubio01 <102053207+rrubio01@users.noreply.github.com>
2022-12-12 15:11:55 -0600

    🎨 REV-2209 Implement variable text handling

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/home/product/smart/SmartContent.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/home/product/smart/SmartContent.kt
@@ -4,6 +4,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.multimoney.data.util.catalog.CreditOnFidoOrFirmStatus.APPROVED
+import com.multimoney.data.util.catalog.CreditOnFidoOrFirmStatus.FAILED
+import com.multimoney.data.util.catalog.CreditOnFidoOrFirmStatus.FIRMED
+import com.multimoney.data.util.catalog.CreditOnFidoOrFirmStatus.OVER_COUNTER
+import com.multimoney.data.util.catalog.CreditOnFidoOrFirmStatus.PENDING
+import com.multimoney.data.util.catalog.CreditOnFidoOrFirmStatus.REJECTED
 import com.multimoney.data.util.catalog.SmartAccountStatus
 import com.multimoney.multimoney.presentation.ui.home.product.ProductViewModel
 import com.multimoney.multimoney.presentation.ui.home.product.smart.uisections.CardInactiveSmartProduct
@@ -13,37 +19,52 @@ import com.multimoney.multimoney.presentation.uielement.ProductBackGroundType
 
 @Composable
 fun SmartContent(viewModel: ProductViewModel, currentPage: Int) {
-    when (viewModel.uiState.userStatus?.infoBankAccount?.status) {
-        SmartAccountStatus.EXIST_IN_CORE.status -> {
-            viewModel.balanceCredit?.balanceAccountSmart?.let {
-                if (it.isNotEmpty()) {
-                    val index = currentPage.minus(viewModel.balanceCredit?.balanceCredit?.size ?: 0)
-                    CustomProductBackground(
-                        modifier = Modifier.padding(horizontal = 16.dp),
-                        type = ProductBackGroundType.Secondary
-                    ) {
-                        CardSmartProduct(
-                            currency = it[index]?.currencyCode ?: "",
-                            profitMonthly = it[index]?.gainedInterest.toString(),
-                            profitTotal = it[index]?.totalBalance.toString()
-                        )
-                    }
+    val index = currentPage.minus(viewModel.balanceCredit?.balanceCredit?.size ?: 0)
+    CustomProductBackground(
+        modifier = Modifier.padding(horizontal = 16.dp),
+        type = ProductBackGroundType.Secondary
+    ) {
+        if (viewModel.uiState.userStatus?.infoBankAccount?.statusFirm?.equals(PENDING.status) == true || viewModel.uiState.userStatus?.infoBankAccount?.statusFirm?.equals(
+                APPROVED.status
+            ) == true || viewModel.uiState.userStatus?.infoBankAccount?.statusFirm?.equals(FIRMED.status) == true || viewModel.uiState.userStatus?.infoBankAccount?.statusFirm?.equals(
+                REJECTED.status
+            ) == true || viewModel.uiState.userStatus?.infoBankAccount?.statusFirm?.equals(
+                OVER_COUNTER.status
+            ) == true || viewModel.uiState.userStatus?.infoBankAccount?.statusFirm?.equals(FAILED.status) == true
+        ) {
+            viewModel.uiState.userStatus?.infoBankAccount?.wording.let {
+                CardInactiveSmartProduct(
+                    it?.textOne.toString(),
+                    it?.textTwo.toString(),
+                    it?.cTA.toString()
+                ) {
+                    // TODO add navigation according to status
+                    viewModel.onUIEvent(ProductViewModel.UIEvent.OnNavigateToSmartOriginationFlow)
                 }
             }
-        }
-        SmartAccountStatus.NO_EXIST.status -> {
-            viewModel.uiState.userStatus?.infoBankAccount?.wording.let {
-                CustomProductBackground(
-                    modifier = Modifier.padding(horizontal = 16.dp),
-                    type = ProductBackGroundType.Secondary
-                ) {
-                    CardInactiveSmartProduct(
-                        it?.textOne.toString(),
-                        it?.textTwo.toString(),
-                        it?.cTA.toString()
-                    ) {
-                        // TODO add navigation according to status
-                        viewModel.onUIEvent(ProductViewModel.UIEvent.OnNavigateToSmartOriginationFlow)
+        } else {
+            when (viewModel.uiState.userStatus?.infoBankAccount?.status) {
+                SmartAccountStatus.EXIST_IN_CORE.status -> {
+                    viewModel.balanceCredit?.balanceAccountSmart?.let {
+                        if (it.isNotEmpty()) {
+                            CardSmartProduct(
+                                currency = it[index]?.currencyCode ?: "",
+                                profitMonthly = it[index]?.gainedInterest.toString(),
+                                profitTotal = it[index]?.totalBalance.toString()
+                            )
+                        }
+                    }
+                }
+                SmartAccountStatus.NO_EXIST.status -> {
+                    viewModel.uiState.userStatus?.infoBankAccount?.wording.let {
+                        CardInactiveSmartProduct(
+                            it?.textOne.toString(),
+                            it?.textTwo.toString(),
+                            it?.cTA.toString()
+                        ) {
+                            // TODO add navigation according to status
+                            viewModel.onUIEvent(ProductViewModel.UIEvent.OnNavigateToSmartOriginationFlow)
+                        }
                     }
                 }
             }

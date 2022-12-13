30e4f0c07c59364fd2fb4f352e822c365b8a9944
Antonio Guerra <anapoleon@applaudostudios.com>
2022-12-13 18:33:04 -0400

    🐛 REV-2786 Fix movements in GT

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/home/HomeViewModel.kt
@@ -375,12 +375,6 @@ class HomeViewModel @Inject constructor(
                             account?.tokenNumber?.toLongOrNull() ?: 0
                         )
                     )
-                    onUIEvent(
-                        OnGetCreditMovements(
-                            uiState.idBrand.toInt(),
-                            uiState.validateUserStatus?.infoCredit?.idLoanClient ?: 0
-                        )
-                    )
                 }
                 // If user has smart activated he can enable crypto
                 productPageList.add(
@@ -414,6 +408,13 @@ class HomeViewModel @Inject constructor(
                 )
             }
         }
+
+        onUIEvent(
+            OnGetCreditMovements(
+                uiState.idBrand.toInt(),
+                uiState.validateUserStatus?.infoCredit?.idLoanClient ?: 0
+            )
+        )
 
         if (uiState.configurationVersion != null && uiState.quickActions != null) {
             uiState = uiState.copy(isLoading = false)

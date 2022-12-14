deb5b8d200be5eb07fd1cf97fa8ff5d0e7d0ae2a
Roberto Escalon <roberto.escalon@applaudostudios.com>
2022-12-14 15:40:30 -0600

    :art: REV-2804 - removing the whole composable

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/home/product/smart/SmartFooter.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/home/product/smart/SmartFooter.kt
@@ -1,18 +1,9 @@
 package com.multimoney.multimoney.presentation.ui.home.product.smart
 
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.unit.sp
-import com.multimoney.multimoney.presentation.theme.MultimoneyTheme
-import com.multimoney.multimoney.presentation.theme.Typography
 
 @Composable
 fun SmartFooter() {
-    // TODO, add smart footer when available, passing an empty text for now
+    // TODO, add smart footer when available, removing the empty composable text for now
     //  due to this ticket: https://akros.atlassian.net/browse/REV-2804
-    Text(
-        text = "",
-        style = Typography.h6.copy(letterSpacing = 0.38.sp),
-        color = MultimoneyTheme.colors.labelText
-    )
 }

e96c6e875fe4e8182646fb1b77a8d936be86574b
Luis Enrique Ramirez <luis_enrique_ramirez@claro.com.do>
2022-12-19 17:51:21 -0400

    sending crypto currencies to ui sections and adding padding to crypto currency list

---
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/home/product/crypto/uisections/CryptoCurrenciesAndMovementsSection.kt
@@ -0,0 +1,62 @@
+package com.multimoney.multimoney.presentation.ui.home.product.crypto.uisections
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.multimoney.domain.model.balance.BalanceCryptoAccountItems
+import com.multimoney.multimoney.R
+import com.multimoney.multimoney.presentation.theme.MultimoneyTheme
+import com.multimoney.multimoney.presentation.theme.Typography
+import com.multimoney.multimoney.presentation.ui.crypto.CurrencyItem
+import com.multimoney.multimoney.presentation.util.MAX_CRYPTO_ITEMS
+
+@Composable
+fun CryptoCurrencies(
+    items: List<BalanceCryptoAccountItems>?
+) {
+    Column(modifier = Modifier
+        .verticalScroll(rememberScrollState())
+        .padding(horizontal = 16.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(horizontal = 4.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                textAlign = TextAlign.Start,
+                text = stringResource(id = R.string.crypto_currencies),
+                style = Typography.body1.copy(fontWeight = FontWeight.SemiBold),
+                color = MultimoneyTheme.colors.labelText
+            )
+            TextButton(onClick = { }) {
+                Text(
+                    textAlign = TextAlign.End,
+                    text = stringResource(id = R.string.crypto_currencies_see_all),
+                    style = Typography.body2.copy(fontWeight = FontWeight.SemiBold),
+                    color = MultimoneyTheme.colors.textLink
+                )
+            }
+        }
+        items?.let {
+            it.take(MAX_CRYPTO_ITEMS).forEach{ item ->
+                CurrencyItem(item = item)
+            }
+        }
+    }
+}

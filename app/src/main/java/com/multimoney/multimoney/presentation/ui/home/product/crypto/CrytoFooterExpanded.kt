0078fdfdf6b80dba6afe04ef037ffce2788ab693
Luis Enrique Ramirez <luis_enrique_ramirez@claro.com.do>
2022-12-19 14:56:09 -0400

    adding empty state flag on Crypto section

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/home/product/crypto/CrytoFooterExpanded.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/home/product/crypto/CrytoFooterExpanded.kt
@@ -3,16 +3,13 @@ package com.multimoney.multimoney.presentation.ui.home.product.crypto
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Divider
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.multimoney.data.util.catalog.Brand
 import com.multimoney.data.util.catalog.CryptoAccountStatus
 import com.multimoney.domain.model.balance.Account

84369fc746b06e8c74dab5a47c7a552920cc1d4f
Jeseap <jsalvador@applaudostudios.com>
2022-12-22 10:01:30 -0600

    ✨ Add navigation to SmartSelectSendingType screen

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/smart/payment/sending/SmartSelectSendingTypeScreen.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/smart/payment/sending/SmartSelectSendingTypeScreen.kt
@@ -1,6 +1,7 @@
 package com.multimoney.multimoney.presentation.ui.smart.payment.sending
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.multimoney.multimoney.presentation.util.NavEvent
 
@@ -10,4 +11,9 @@ fun SmartSelectSendingTypeScreen(
     onPopBackStack: (NavEvent.PopBackStack) -> Unit = {},
     viewModel: SmartSelectSendingTypeViewModel = hiltViewModel()
 ){
+    LaunchedEffect(true) {
+        viewModel.apply {
+            executeNavigation(onNavigate = onNavigate, onPopBackStack = onPopBackStack)
+        }
+    }
 }

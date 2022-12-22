b370ae6c5eb594aa17f6b43e61c9c40a3003268f
Jeseap <jsalvador@applaudostudios.com>
2022-12-22 14:17:21 -0600

    ✨ Add SendingTypeOptions composable

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/smart/payment/sending/SmartSelectSendingTypeScreen.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/smart/payment/sending/SmartSelectSendingTypeScreen.kt
@@ -1,8 +1,19 @@
 package com.multimoney.multimoney.presentation.ui.smart.payment.sending
 
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.multimoney.multimoney.presentation.theme.MultimoneyTheme
+import com.multimoney.multimoney.presentation.uielement.CustomInfoButton
+import com.multimoney.multimoney.presentation.uielement.TopNavBar
 import com.multimoney.multimoney.presentation.util.NavEvent
 
 @Composable
@@ -16,4 +27,39 @@ fun SmartSelectSendingTypeScreen(
             executeNavigation(onNavigate = onNavigate, onPopBackStack = onPopBackStack)
         }
     }
+
+    Column(
+        modifier = Modifier
+            .background(MultimoneyTheme.colors.background)
+            .fillMaxSize()
+    ) {
+        TopNavBar(
+            onLeftButtonClick = {  },
+            isRightButtonVisible = false
+        )
+        SendingTypeOptions()
+    }
+    BackHandler {}
+}
+
+@Composable
+fun SendingTypeOptions(
+
+){
+    Column {
+        Text(
+            modifier = Modifier.padding(top = 32.dp),
+            text = "Enviar dinero a"
+        )
+        CustomInfoButton(
+            title = ""
+        )
+        CustomInfoButton(
+            title = ""
+        )
+        CustomInfoButton(
+            title = ""
+        )
+    }
+
 }

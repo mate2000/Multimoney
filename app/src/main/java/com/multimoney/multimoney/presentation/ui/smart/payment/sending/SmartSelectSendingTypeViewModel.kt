8f26458805a08afd7106d46c7d469f2b7796040c
Jeseap <jsalvador@applaudostudios.com>
2022-12-22 18:30:42 -0600

    💄 Add UIEvent class

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/smart/payment/sending/SmartSelectSendingTypeViewModel.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/smart/payment/sending/SmartSelectSendingTypeViewModel.kt
@@ -1,7 +1,13 @@
 package com.multimoney.multimoney.presentation.ui.smart.payment.sending
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import com.multimoney.multimoney.presentation.base.BaseViewModel
+import com.multimoney.multimoney.presentation.navigation.Screen
+import com.multimoney.multimoney.presentation.ui.smart.payment.cards.SmartPaymentCardsViewModel
+import com.multimoney.multimoney.presentation.ui.smart.payment.sending.SmartSelectSendingTypeViewModel.UIEvent.OnNavigateBack
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -9,5 +15,26 @@ import javax.inject.Inject
 class SmartSelectSendingTypeViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle
 ) : BaseViewModel(true) {
+
+    // uiState
+    var uiState by mutableStateOf(SmartPaymentCardsViewModel.UIState())
+        private set
+
+    fun onUIEvent(uiEvent: UIEvent) {
+        when (uiEvent) {
+            is OnNavigateBack -> onNavigateBack()
+        }
+    }
+
+    private fun onNavigateBack() {
+        navigateBack(
+            popTo = Screen.HomeScreen.route,
+            isRestart = false
+        )
+    }
+
+    sealed class UIEvent {
+        object OnNavigateBack : UIEvent()
+    }
 
 }

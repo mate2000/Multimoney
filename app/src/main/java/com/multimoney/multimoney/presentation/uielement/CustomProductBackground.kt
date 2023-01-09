25544c3f33fad88f477ac64e3927da76a0be4016
Oscar Lizama <olizama@applaudostudios.com>
2023-01-09 16:40:20 -0600

    Modify the composable function 'CustomProductBackground' according to expected design

--- app/src/main/java/com/multimoney/multimoney/presentation/uielement/CustomProductBackground.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/uielement/CustomProductBackground.kt
@@ -1,41 +1,26 @@
 package com.multimoney.multimoney.presentation.uielement
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.multimoney.multimoney.R.drawable
-import com.multimoney.multimoney.presentation.theme.ComplementaryOne500
-import com.multimoney.multimoney.presentation.theme.ComplementaryTwo500
-import com.multimoney.multimoney.presentation.theme.GradientComplementaryOne
-import com.multimoney.multimoney.presentation.theme.GradientComplementaryTwo
-import com.multimoney.multimoney.presentation.theme.GradientPrimary
-import com.multimoney.multimoney.presentation.theme.GradientSecondary
-import com.multimoney.multimoney.presentation.theme.GradientTertiary
-import com.multimoney.multimoney.presentation.theme.Primary500
-import com.multimoney.multimoney.presentation.theme.Primary600
-import com.multimoney.multimoney.presentation.theme.Secondary500
-import com.multimoney.multimoney.presentation.theme.Tertiary500
-import com.multimoney.multimoney.presentation.theme.ComplementaryTwo400
-import com.multimoney.multimoney.presentation.uielement.ProductBackGroundType.ComplementaryOne
 import com.multimoney.multimoney.presentation.uielement.ProductBackGroundType.ComplementaryTwo
 import com.multimoney.multimoney.presentation.uielement.ProductBackGroundType.Primary
 import com.multimoney.multimoney.presentation.uielement.ProductBackGroundType.Secondary
-import com.multimoney.multimoney.presentation.uielement.ProductBackGroundType.Tertiary
+import com.multimoney.multimoney.presentation.util.coloredShadow
 
 const val SIXTY_PERCENT = 0.90
 @Preview
@@ -51,91 +36,48 @@ fun CustomProductBackground(
     val screenWidth = configuration.screenWidthDp.dp
     val startOffset = screenWidth.value * SIXTY_PERCENT
 
-    val gradientOneColor: Color
-    val gradientTwoColor: Color
-    val gradientThreeColor: Color
-
-    when (type) {
+    val shadowCardColor = Color(0xFF4E86EF)
+    val cardResourceId: Int = when (type) {
         Primary -> {
-            if (isSystemInDarkTheme()) {
-                gradientOneColor = Primary500
-                gradientTwoColor = Primary500
-                gradientThreeColor = Color(0xFF81DB65)
-            } else {
-                gradientOneColor = Primary500
-                gradientTwoColor = Primary600
-                gradientThreeColor = Primary500
-            }
+            drawable.bg_card_credit
         }
         Secondary -> {
-            if (isSystemInDarkTheme()) {
-                gradientOneColor = Secondary500
-                gradientTwoColor = Secondary500
-                gradientThreeColor = GradientSecondary
-            } else {
-                gradientOneColor = Secondary500
-                gradientTwoColor = Secondary500
-                gradientThreeColor = GradientSecondary
-            }
-        }
-        Tertiary -> {
-            if (isSystemInDarkTheme()) {
-                gradientOneColor = Tertiary500
-                gradientTwoColor = Tertiary500
-                gradientThreeColor = GradientTertiary
-            } else {
-                gradientOneColor = Tertiary500
-                gradientTwoColor = Tertiary500
-                gradientThreeColor = GradientTertiary
-            }
-        }
-        ComplementaryOne -> {
-            if (isSystemInDarkTheme()) {
-                gradientOneColor = ComplementaryOne500
-                gradientTwoColor = ComplementaryOne500
-                gradientThreeColor = GradientComplementaryOne
-            } else {
-                gradientOneColor = ComplementaryOne500
-                gradientTwoColor = ComplementaryOne500
-                gradientThreeColor = GradientComplementaryOne
-            }
+            drawable.bg_card_smart
         }
         ComplementaryTwo -> {
-            if (isSystemInDarkTheme()) {
-                gradientOneColor = ComplementaryTwo500
-                gradientTwoColor = ComplementaryTwo500
-                gradientThreeColor = ComplementaryTwo400.copy(.8f)
-            } else {
-                gradientOneColor = ComplementaryTwo500
-                gradientTwoColor = ComplementaryTwo500
-                gradientThreeColor = ComplementaryTwo400.copy(.8f)
-            }
+            drawable.bg_card_crypto
+        }
+        else -> {
+            drawable.bg_card_credit
         }
     }
 
+    Row(
+        modifier = Modifier
+            .coloredShadow(shadowCardColor)
+            .padding(bottom = 16.dp)
+    ) {
+        Box(modifier = modifier) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(24.dp))
+                    .blur(0.24.dp)
 
-    Box(modifier = modifier) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-                .clip(RoundedCornerShape(24.dp))
-                .background(
-                    brush = Brush.linearGradient(
-                        colors = listOf(gradientOneColor, gradientTwoColor, gradientThreeColor),
-                        start = Offset(-startOffset.toFloat(), Float.POSITIVE_INFINITY),
-                        end = Offset(Float.POSITIVE_INFINITY, 0f)
-                    )
+            ) {
+                CustomImage(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    drawableResource = cardResourceId,
+                    contentScale = ContentScale.FillBounds
                 )
-                .blur(0.24.dp)
-
-        ) {
-            CustomImage(
-                drawableResource = drawable.ic_swipe_indicator, modifier = Modifier
-                    .padding(top = 8.dp)
-                    .align(Alignment.TopCenter)
-            )
-            content()
+                CustomImage(
+                    drawableResource = drawable.ic_swipe_indicator, modifier = Modifier
+                        .padding(top = 8.dp)
+                        .align(Alignment.TopCenter)
+                )
+                content()
+            }
         }
     }
 }

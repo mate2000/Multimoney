079091a099141697c1a3c461ceefb906ba8d0c91
Luis Enrique Ramirez <leramirez@applaudostudios.com>
2023-01-14 13:49:20 -0400

    adding BalanceTextView.kt component to turn text smaller after decimals

---
+++ app/src/main/java/com/multimoney/multimoney/presentation/uielement/BalanceTextView.kt
@@ -0,0 +1,59 @@
+package com.multimoney.multimoney.presentation.uielement
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import com.multimoney.multimoney.presentation.util.catalog.CurrencyType
+
+/**
+ * @param modifier - modifier only for BalanceText
+ * @param balanceText - text to display with the balance
+ * @param splitWith - string separator default is "."
+ * @param currencySymbol - currency symbol to display
+ * @param currencyStyle - style for currency main text
+ * @param currencyDecimalStyle - style for currency decimal text
+ *
+ * **/
+
+const val DEFAULT_SPLIT_WITH = "."
+
+@Composable
+fun BalanceTextView(
+    modifier: Modifier = Modifier,
+    balanceText: String,
+    splitWith: String = DEFAULT_SPLIT_WITH,
+    currencySymbol: String = CurrencyType.Dollar.symbol,
+    currencyStyle: TextStyle,
+    currencyDecimalStyle: TextStyle
+) {
+    val splitText = balanceText.split(splitWith)
+
+    Row(
+        modifier = Modifier.wrapContentSize()
+    ) {
+        Text(
+            modifier = modifier,
+            text = buildAnnotatedString {
+                append(currencySymbol)
+                append(splitText[0])
+            },
+            style = currencyStyle,
+        )
+        Box(contentAlignment = Alignment.Center) {
+            Text(
+                text = buildAnnotatedString {
+                    append(splitWith)
+                    append(splitText[1])
+                },
+                style = currencyDecimalStyle
+            )
+        }
+    }
+}

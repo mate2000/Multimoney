896b9b7fa83dd9022adc72b7bb3dc023abcb2c47
Oscar Lizama <olizama@applaudostudios.com>
2023-01-13 20:48:47 -0600

    Modify size and padding of icon actions of the home NavBar

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/home/product/ProductScreen.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/home/product/ProductScreen.kt
@@ -296,19 +296,22 @@ fun TipsAndOffer(
                     color = MultimoneyTheme.colors.labelText
                 )
             }
-            Row {
+            Row(modifier = Modifier.padding(end = 8.dp)) {
                 Icon(
                     painter = painterResource(R.drawable.ic_notification),
-                    modifier = Modifier.clickable {
-                        // todo action
-                    },
+                    modifier = Modifier
+                        .size(32.dp)
+                        .clickable {
+                            // todo action
+                        },
                     contentDescription = "",
                     tint = MultimoneyTheme.colors.iconColor
                 )
                 Icon(
                     painter = painterResource(R.drawable.ic_profile),
                     modifier = Modifier
-                        .padding(start = 16.dp, end = 2.dp)
+                        .padding(start = 16.dp)
+                        .size(32.dp)
                         .clickable {
                             viewModel.onUIEvent(OnNavigateToProfileScreen)
                         },

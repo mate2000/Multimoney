d5dd95850976e63ba1564111dcedb3297776a9f3
Oscar Lizama <olizama@applaudostudios.com>
2023-01-16 16:35:27 -0600

    Modify leading icon of the monthly income TextField

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/smart/origination/sourceincome/options/ownbusinessinpartnership/OwnBusinessInPartnershipScreen.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/smart/origination/sourceincome/options/ownbusinessinpartnership/OwnBusinessInPartnershipScreen.kt
@@ -185,7 +185,7 @@ fun OwnBusinessOnPersonalBasisContent(
                 R.string.smart_decimal_income_placeholder,
                 currencySymbol
             ),
-            leadingIcon = R.drawable.ic_money_gray,
+            leadingIcon = R.drawable.ic_quick_action_money,
             customTransformation = formatDecimalMoney(currencySymbol),
             isRequired = true,
             isRequiredMessage = stringResource(R.string.smart_business_personal_basis_income_required_message)

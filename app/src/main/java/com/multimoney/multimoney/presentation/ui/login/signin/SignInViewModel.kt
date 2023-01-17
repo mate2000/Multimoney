853f492e3ea776f8c869ffa3a1cabda7c5fc2cdf
Dsanchez1992 <103954910+Dsanchez1992@users.noreply.github.com>
2023-01-17 13:00:05 -0600

    :fire: REV-3054 Remove test code

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/login/signin/SignInViewModel.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/login/signin/SignInViewModel.kt
@@ -24,6 +24,7 @@ import com.multimoney.multimoney.R
 import com.multimoney.multimoney.R.string
 import com.multimoney.multimoney.presentation.base.BaseViewModel
 import com.multimoney.multimoney.presentation.navigation.Screen
+import com.multimoney.multimoney.presentation.navigation.navgraph.PREVIOUS_SCREEN
 import com.multimoney.multimoney.presentation.ui.login.signin.SignInViewModel.UIEvent.OnCallCognitoSignIn
 import com.multimoney.multimoney.presentation.ui.login.signin.SignInViewModel.UIEvent.OnCloseDialog
 import com.multimoney.multimoney.presentation.ui.login.signin.SignInViewModel.UIEvent.OnFingerprintCheckedChanged
@@ -44,6 +45,7 @@ import com.multimoney.multimoney.presentation.util.checkIfEmulator
 import com.multimoney.multimoney.presentation.util.getAppVersion
 import com.multimoney.multimoney.presentation.util.getDeviceBrand
 import com.multimoney.multimoney.presentation.util.getDeviceModel
+import com.multimoney.multimoney.presentation.util.getNavParam
 import com.multimoney.multimoney.presentation.util.isEmailValid
 import com.multimoney.multimoney.util.BiometricHelper
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -417,15 +419,11 @@ class SignInViewModel @Inject constructor(
         )
     }
 
-//    private fun onNavigateToForgotPassword() = navigateTo(
-//        route = Screen.RequestForgotPassword.baseRoute
-//            .plus(
-//                getNavParam(PREVIOUS_SCREEN, Screen.SignInScreen.route)
-//            )
-//    )
-
     private fun onNavigateToForgotPassword() = navigateTo(
-        route = Screen.TestScreen.route
+        route = Screen.RequestForgotPassword.baseRoute
+            .plus(
+                getNavParam(PREVIOUS_SCREEN, Screen.SignInScreen.route)
+            )
     )
 
     private fun onShowBiometricSignInChanged(value: Boolean) {

c07b56841b9ad4398ce6ce1b1d5edb01c632c89e
Oscar Lizama <olizama@applaudostudios.com>
2023-01-17 08:33:00 -0600

    Remove unused import directive

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/home/product/ProductViewModel.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/home/product/ProductViewModel.kt
@@ -1,7 +1,6 @@
 package com.multimoney.multimoney.presentation.ui.home.product
 
 import android.content.Context
-import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue

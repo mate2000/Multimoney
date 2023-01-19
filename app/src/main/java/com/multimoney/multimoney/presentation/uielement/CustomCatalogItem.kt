59f6811f9859f9b03eeb47d7addd5b19930f6b27
Oscar Lizama <olizama@applaudostudios.com>
2023-01-18 19:22:44 -0600

    Remove unused import directives

--- app/src/main/java/com/multimoney/multimoney/presentation/uielement/CustomCatalogItem.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/uielement/CustomCatalogItem.kt
@@ -9,15 +9,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier

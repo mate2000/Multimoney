54ef8397927a7803d2f781ac632b80c355283670
Luis Enrique Ramirez <leramirez@applaudostudios.com>
2023-01-23 21:22:32 -0400

    fixing filter chip is showing with sv account

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/crypto/market/MarketScreen.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/crypto/market/MarketScreen.kt
@@ -150,6 +150,7 @@ fun MarketScreenContent(
                         searchQuery = searchQuery,
                         selectedFilter = selectedFilter,
                         sheetState = state,
+                        brandIsCr = marketViewModel.uiState.idBrand == Brand.CostaRica.id,
                         onCurrencyItemClick = { cryptoCurrency ->
                             marketViewModel.onUIEvent(OnSetAssetBeforeNavigation(
                                 asset = cryptoCurrency.baseAsset,
@@ -284,6 +285,7 @@ fun ListOfCoinsSection(
     searchQuery: MutableState<String>,
     selectedFilter: MutableState<String>,
     sheetState: ModalBottomSheetState,
+    brandIsCr: Boolean = false,
     onCurrencyItemClick: (MarketCryptoCoin) -> Unit
 ) {
 
@@ -302,8 +304,10 @@ fun ListOfCoinsSection(
     LazyColumn(
         contentPadding = PaddingValues(vertical = 8.dp),
     ) {
-        item {
-            FilterSection(selectedFilter = selectedFilter, sheetState = sheetState)
+        if (brandIsCr) {
+            item {
+                FilterSection(selectedFilter = selectedFilter, sheetState = sheetState)
+            }
         }
         items(filteredList) { cryptoCoin ->
             MarketCurrencyItem(

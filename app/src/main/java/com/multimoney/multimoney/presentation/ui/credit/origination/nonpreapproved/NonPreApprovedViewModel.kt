c0a13901ea8f18ea10406ada892c4c89d2380b32
José Ricardo <jciterio@applaudostudios.com>
2023-02-07 14:38:16 -0400

    🐛REV-3341 - Errores en funcionalidad y diseño.
    fix navigation to credit origination

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/credit/origination/nonpreapproved/NonPreApprovedViewModel.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/credit/origination/nonpreapproved/NonPreApprovedViewModel.kt
@@ -336,7 +336,7 @@ class NonPreApprovedViewModel @Inject constructor(
         route = "${Screen.CreditScreen.baseRoute}/${idBrand ?: 0}/${pkUser ?: 0}/${identification.orEmpty()}/${email.orEmpty()}/${lastStep ?: CreditStep.One.id}/" +
                 "${idUserRequest ?: 0}/${firstName.orEmpty()}/" +
                 "${lastName.orEmpty()}/${statusOnfido.orEmpty()}/" +
-                "${statusEvicertia.orEmpty()}/${idPrint ?: 0}" +
+                "${statusEvicertia.orEmpty()}/${idPrint ?: 0}/" +
                 "${crosseling ?: false}",
         popTo = Screen.NonPreApprovedScreen.route
     )

8feb345e32085ff3ebb069f3efb008f532bc194e
Dsanchez1992 <103954910+Dsanchez1992@users.noreply.github.com>
2023-02-09 11:17:06 -0600

    :bug: REV-3511 change texts and refactor view to show preview

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/credit/origination/evisertiaandonfidoerrors/OnfidoAndEvicertiaErrorsScreen.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/credit/origination/evisertiaandonfidoerrors/OnfidoAndEvicertiaErrorsScreen.kt
@@ -2,7 +2,9 @@ package com.multimoney.multimoney.presentation.ui.credit.origination.evisertiaan
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.multimoney.data.util.catalog.Brand.ElSalvador
 import com.multimoney.data.util.catalog.Brand.Guatemala
 import com.multimoney.multimoney.R.drawable
 import com.multimoney.multimoney.R.string
@@ -19,44 +21,64 @@ import com.multimoney.multimoney.presentation.util.catalog.OnfidoAndEvicertiaErr
 fun OnfidoAndEvicertiaErrorsScreen(
     onPopAndNavigate: (NavEvent.PopAndNavigate) -> Unit = {},
     onPopBackStack: (NavEvent.PopBackStack) -> Unit = {},
-    viewModel: OnfidoAndEvicertiaErrorsViewModel = hiltViewModel()
+    viewModel: OnfidoAndEvicertiaErrorsViewModel = hiltViewModel(),
 ) {
     LaunchedEffect(true) {
         viewModel.executeNavigation(onPopAndNavigate = onPopAndNavigate, onPopBackStack = onPopBackStack)
     }
 
-    when (viewModel.uiState.error) {
+    OnfidoAndEvicertiaErrorsContent(
+        viewModel.uiState.error,
+        viewModel.idBrand,
+        onNavigateHome = {
+            viewModel.onUIEvent(OnNavigateToHome)
+        },
+        onNavigateToOnfido = {
+            viewModel.onUIEvent(OnNavigateToOnfidoProcess)
+        },
+    )
+}
+
+@Preview
+@Composable
+fun OnfidoAndEvicertiaErrorsContent(
+    error: String = EVICERTIA_REJECTED_FIRST_TIME.value,
+    idBrand: Int = ElSalvador.id,
+    onNavigateHome: () -> Unit = {},
+    onNavigateToOnfido: () -> Unit = {},
+) {
+    when (error) {
         EVICERTIA_REJECTED_FIRST_TIME.value -> {
             AlertResult(
                 iconResource = drawable.ic_alert,
                 titleResource = string.sign_document_reject_title,
-                descriptionResource = if (viewModel.idBrand == Guatemala.id) string.sign_document_reject_description_gt else string.sign_document_reject_description,
-                buttonTextResource = string.understood,
+                descriptionResource = if (idBrand == Guatemala.id) string.sign_document_reject_description_gt else string.sign_document_reject_description,
+                buttonTextResource = string.sign,
                 isLeftButtonVisible = false,
-                onRightButtonClick = { viewModel.onUIEvent(OnNavigateToHome) },
-                onButtonClick = { viewModel.onUIEvent(OnNavigateToHome) }
+                onRightButtonClick = { onNavigateHome() },
+                onButtonClick = { onNavigateHome() },
             )
         }
         EVICERTIA_REJECTED_SECOND_TIME.value -> {
             AlertResult(
                 iconResource = drawable.ic_alert,
                 titleResource = string.sign_credit_max_attempts_title,
-                descriptionResource = if (viewModel.idBrand == Guatemala.id) string.sign_credit_max_attempts_message_gt else string.sign_credit_max_attempts_message,
+                descriptionResource = if (idBrand == Guatemala.id) string.sign_credit_max_attempts_message_gt else string.sign_credit_max_attempts_message,
                 buttonTextResource = string.understood,
                 isLeftButtonVisible = false,
-                onRightButtonClick = { viewModel.onUIEvent(OnNavigateToHome) },
-                onButtonClick = { viewModel.onUIEvent(OnNavigateToHome) }
+                onRightButtonClick = { onNavigateHome() },
+                onButtonClick = { onNavigateHome() },
             )
         }
         ONFIDO_REJECTED_FIRST_TIME.value -> {
             AlertResult(
                 iconResource = drawable.ic_error_symbol,
                 titleResource = string.onfido_rejected_first_time_title,
-                descriptionResource = if (viewModel.idBrand == Guatemala.id) string.onfido_rejected_first_time_subtitle_gt else string.onfido_rejected_first_time_subtitle,
+                descriptionResource = if (idBrand == Guatemala.id) string.onfido_rejected_first_time_subtitle_gt else string.onfido_rejected_first_time_subtitle,
                 buttonTextResource = string.onfido_rejected_action,
                 isLeftButtonVisible = false,
-                onRightButtonClick = { viewModel.onUIEvent(OnNavigateToHome) },
-                onButtonClick = { viewModel.onUIEvent(OnNavigateToOnfidoProcess) }
+                onRightButtonClick = { onNavigateHome() },
+                onButtonClick = { onNavigateToOnfido() },
             )
         }
         ONFIDO_REJECTED_SECOND_TIME.value -> {
@@ -66,8 +88,8 @@ fun OnfidoAndEvicertiaErrorsScreen(
                 descriptionResource = string.onfido_rejected_second_time_subtitle,
                 buttonTextResource = string.onfido_rejected_action,
                 isLeftButtonVisible = false,
-                onRightButtonClick = { viewModel.onUIEvent(OnNavigateToHome) },
-                onButtonClick = { viewModel.onUIEvent(OnNavigateToHome) }
+                onRightButtonClick = { onNavigateHome() },
+                onButtonClick = { onNavigateHome() },
             )
         }
     }

63752bb670f8d01532fb20dbe02b1513bef47163
Josue Torres <jatorres@applaudostudios.com>
2023-02-10 17:26:40 -0600

    Create composable for CryptoSendAmount flow

---
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/crypto/send/cryptoamount/CryptoSendAmountScreen.kt
@@ -0,0 +1,148 @@
+package com.multimoney.multimoney.presentation.ui.crypto.send.cryptoamount
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.rememberAsyncImagePainter
+import com.multimoney.multimoney.R
+import com.multimoney.multimoney.presentation.theme.LocalMultimoneyColors
+import com.multimoney.multimoney.presentation.theme.MultimoneyTheme
+import com.multimoney.multimoney.presentation.theme.Typography
+import com.multimoney.multimoney.presentation.ui.crypto.purchase.buycurrency.AmountInputSection
+import com.multimoney.multimoney.presentation.ui.crypto.send.CryptoSendSharedViewModel
+import com.multimoney.multimoney.presentation.uielement.CustomButton
+import com.multimoney.multimoney.presentation.uielement.CustomInformativeText
+import com.multimoney.multimoney.presentation.util.calculateAssetEstimated
+
+@Composable
+@OptIn(ExperimentalComposeUiApi::class)
+fun CryptoSendAmountScreen(
+    viewModel: CryptoSendAmountViewModel = hiltViewModel(),
+    sharedViewModel: CryptoSendSharedViewModel
+) {
+    val focusRequester = remember { FocusRequester() }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    LaunchedEffect(key1 = true) {
+        viewModel.onUIEvent(
+            CryptoSendAmountViewModel.UIEvent.OnSetUserData(
+                pkUser = sharedViewModel.pkUser.toInt(),
+                asset = sharedViewModel.uiState.asset,
+                cryptoNetwork = sharedViewModel.uiState.cryptoNetwork,
+                idBrand = sharedViewModel.idBrand,
+                user = sharedViewModel.email,
+                identification = sharedViewModel.identification,
+                assetImageUrl = sharedViewModel.uiState.assetImg,
+                destinationAddress = sharedViewModel.uiState.destinationAddress,
+                currentBalanceInDollar = sharedViewModel.uiState.currencyDollarBalance,
+                currentCryptoBalance = calculateAssetEstimated(
+                    sharedViewModel.uiState.currencyDollarBalance.toString(),
+                    sharedViewModel.uiState.cryptoCurrencyPrice
+                ).toDouble()
+            )
+        )
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MultimoneyTheme.colors.background)
+            .padding(16.dp)
+    ) {
+        Text(
+            text = stringResource(
+                id = R.string.crypto_amount_title,
+                sharedViewModel.uiState.assetDescription
+            ),
+            style = Typography.h6.copy(
+                fontWeight = FontWeight.SemiBold,
+                color = LocalMultimoneyColors.current.titleText,
+                lineHeight = 24.sp,
+                fontSize = 20.sp
+            ),
+            textAlign = TextAlign.Left
+        )
+
+        CustomInformativeText(
+            leadingIcon = R.drawable.ic_information,
+            text = stringResource(
+                id = R.string.crypto_amount_info_msg
+            ),
+            textStyle = Typography.body2.copy(color = MultimoneyTheme.colors.titleText),
+            alignmentVertical = Alignment.Top
+        )
+
+        Row(
+            modifier = Modifier.padding(top = 5.dp, bottom = 5.dp)
+        ) {
+            Image(
+                painter = rememberAsyncImagePainter(model = sharedViewModel.uiState.assetImg),
+                contentDescription = sharedViewModel.uiState.assetDescription
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(
+                modifier = Modifier.padding(top = 2.dp),
+                text = stringResource(
+                    id = R.string.crypto_available_dollar_amount_to_bitcoin,
+                    sharedViewModel.uiState.currencyDollarBalance,
+                    calculateAssetEstimated(
+                        sharedViewModel.uiState.currencyDollarBalance.toString(),
+                        sharedViewModel.uiState.cryptoCurrencyPrice
+                    ),
+                    sharedViewModel.uiState.asset
+                ),
+                style = Typography.body2,
+                color = MultimoneyTheme.colors.titleText,
+                textAlign = TextAlign.Start
+            )
+        }
+
+        AmountInputSection(
+            asset = sharedViewModel.uiState.asset,
+            currencyPrice = sharedViewModel.uiState.cryptoCurrencyPrice,
+            isError = viewModel.uiState.isError,
+            errorText = viewModel.uiState.error,
+            quoteAmount = viewModel.uiState.quoteAmount,
+            baseAmount = viewModel.uiState.baseAmount,
+            isTransformationCurrency = viewModel.uiState.isTransformationCurrency,
+            keyboardController = keyboardController,
+            focusRequester = focusRequester,
+            onAmountChanged = { amount ->
+                viewModel.onUIEvent(CryptoSendAmountViewModel.UIEvent.ValidateAmountInput(amount))
+            }
+        )
+
+        Spacer(Modifier.weight(1f))
+
+        CustomButton(
+            modifier = Modifier
+                .padding(top = 12.dp, bottom = 40.dp)
+                .fillMaxWidth()
+                .height(48.dp),
+            onClick = {
+            },
+            enable = false
+        )
+    }
+}

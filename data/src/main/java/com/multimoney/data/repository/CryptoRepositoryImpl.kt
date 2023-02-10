1b84d68814fa52cbbf43e6dc50298216c75fc0c6
Josue Torres <jatorres@applaudostudios.com>
2023-02-10 17:18:16 -0600

    Create impl for CryptoRepository

--- data/src/main/java/com/multimoney/data/repository/CryptoRepositoryImpl.kt
+++ data/src/main/java/com/multimoney/data/repository/CryptoRepositoryImpl.kt
@@ -14,6 +14,7 @@ import com.multimoney.domain.model.crypto.CryptoCurrencyNews
 import com.multimoney.domain.model.crypto.GetHistoricalClientBalance
 import com.multimoney.domain.model.crypto.GetHistoricalCurrencyPrices
 import com.multimoney.domain.model.crypto.GetListOfAvailableCryptoCoins
+import com.multimoney.domain.model.crypto.GetTransferFeeData
 import com.multimoney.domain.model.crypto.PricesQuoteAndCommissionData
 import com.multimoney.domain.model.crypto.ValidateDepositAddressResponse
 import com.multimoney.domain.model.util.MultimoneyResult
@@ -197,6 +198,25 @@ class CryptoRepositoryImpl @Inject constructor(
             identification,
             market,
             address
+        ),
+        apolloCallMapper = { data -> Success(data.mapToDomainModel()) }
+    )
+
+    override suspend fun getTransferCommission(
+        user: String,
+        idBrand: Int,
+        destinationAddress: String,
+        asset: String,
+        cryptoNetwork: String,
+        amount: Double
+    ): Flow<MultimoneyResult<GetTransferFeeData>> = fetchData(
+        apolloCall = graphqlApi.getTransferCommission(
+            user,
+            idBrand,
+            destinationAddress,
+            asset,
+            cryptoNetwork,
+            amount
         ),
         apolloCallMapper = { data -> Success(data.mapToDomainModel()) }
     )

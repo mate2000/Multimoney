36693f597270cdc1c71900656e82272d83eae98f
Elias Eguizabal <emendoza@applaudostudios.com>
2023-02-09 18:02:59 -0600

    :bug: REV-3617 Fix pop screen when execute navigation

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/smart/origination/onfido/SmartOnfidoViewModel.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/smart/origination/onfido/SmartOnfidoViewModel.kt
@@ -241,7 +241,7 @@ class SmartOnfidoViewModel @Inject constructor(
     private fun onNavigateToSignDocumentScreen(signDocumentStep: String) {
         popAndNavigateTo(
             route = "${Screen.SmartSignScreen.baseRoute}/$signDocumentStep/$evicertiaUrl/$idPrint/$idBrand/$pkUser/$identification/$email/$idUserRequest/$firstName/$lastName/${true}/$globalId/$user",
-            popTo = Screen.HomeScreen.route
+            popTo = Screen.SmartOnfidoScreen.route
         )
     }
 

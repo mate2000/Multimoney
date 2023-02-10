31abaf815514d8c5302050922afa25bb369a8753
Josue Torres <jatorres@applaudostudios.com>
2023-02-10 17:08:53 -0600

    Create repository CryptoRepository

--- domain/src/main/java/com/multimoney/domain/repository/CryptoRepository.kt
+++ domain/src/main/java/com/multimoney/domain/repository/CryptoRepository.kt
@@ -8,6 +8,7 @@ import com.multimoney.domain.model.crypto.CryptoCurrencyNews
 import com.multimoney.domain.model.crypto.GetHistoricalClientBalance
 import com.multimoney.domain.model.crypto.GetHistoricalCurrencyPrices
 import com.multimoney.domain.model.crypto.GetListOfAvailableCryptoCoins
+import com.multimoney.domain.model.crypto.GetTransferFeeData
 import com.multimoney.domain.model.crypto.PricesQuoteAndCommissionData
 import com.multimoney.domain.model.crypto.ValidateDepositAddressResponse
 import com.multimoney.domain.model.util.MultimoneyResult
@@ -98,4 +99,13 @@ interface CryptoRepository {
         market: String,
         address: String
     ): Flow<MultimoneyResult<ValidateDepositAddressResponse>>
+
+    suspend fun getTransferCommission(
+        user: String,
+        idBrand: Int,
+        destinationAddress: String,
+        asset: String,
+        cryptoNetwork: String,
+        amount: Double
+    ): Flow<MultimoneyResult<GetTransferFeeData>>
 }

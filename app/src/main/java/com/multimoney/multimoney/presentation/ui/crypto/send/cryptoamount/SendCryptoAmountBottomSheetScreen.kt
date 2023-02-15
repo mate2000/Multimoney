4ace18143f9b6fc33cc8ca6ad71ab021fc5e9ac4
Gustavo Müller <gmuller@applaudostudios.com>
2023-02-15 14:21:55 -0600

    :lipstick: Refactor bottomSheet to take data from viewmodel

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/crypto/send/cryptoamount/SendCryptoAmountBottomSheetScreen.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/crypto/send/cryptoamount/SendCryptoAmountBottomSheetScreen.kt
@@ -1,6 +1,9 @@
-package com.multimoney.multimoney.presentation.ui.crypto.send.amount
+package com.multimoney.multimoney.presentation.ui.crypto.send.cryptoamount
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,20 +11,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetState
-import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.multimoney.multimoney.R
 import com.multimoney.multimoney.presentation.theme.MultimoneyTheme
@@ -29,31 +30,50 @@ import com.multimoney.multimoney.presentation.theme.Typography
 import com.multimoney.multimoney.presentation.uielement.CustomButton
 import com.multimoney.multimoney.presentation.uielement.CustomButtonType
 import com.multimoney.multimoney.presentation.uielement.CustomLabeledInfoText
-import com.multimoney.multimoney.presentation.uielement.CustomModalBottomSheet
+import com.multimoney.multimoney.presentation.util.roundToEightDecimalPlaces
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
-@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SendCryptoAmountBottomSheetScreen(
-//    viewModel: ,
-//    coroutineScope: CoroutineScope,
-//    modalBottomSheetState: ModalBottomSheetState
+    viewModel: CryptoSendAmountViewModel,
+    coroutineScope: CoroutineScope,
+    modalBottomSheetState: ModalBottomSheetState
 ) {
-    CustomModalBottomSheet(
-        title = R.string.crypto_send_amount_bottom_sheet_title,
-        closeIcon = R.drawable.ic_close_bottom_sheet,
-        modalBottomSheetState = ModalBottomSheetState(ModalBottomSheetValue.Expanded), //TODO get from param
-        coroutineScope = rememberCoroutineScope(), //TODO get from param
-        shape = RoundedCornerShape(16.dp)
+    Column(modifier = Modifier
+        .wrapContentSize()
+        .background(color = MultimoneyTheme.colors.creditDetailBackground)
     ) {
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 16.dp, horizontal = 24.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = stringResource(id = R.string.crypto_send_amount_bottom_sheet_title),
+                style = Typography.subtitle1.copy(
+                    color = MultimoneyTheme.colors.text,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+            Image(
+                modifier = Modifier.clickable {
+                    coroutineScope.launch { modalBottomSheetState.hide() }
+                },
+                painter = painterResource(id = R.drawable.ic_close_bottom_sheet),
+                contentDescription = null
+            )
+        }
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 16.dp)
+                .padding(top = 16.dp, bottom = 24.dp, start = 24.dp, end = 24.dp)
         ) {
             Text(
                 modifier = Modifier.fillMaxWidth(),
-                text = "0.000027 BTC", //TODO get from viewModel
+                text = "${viewModel.uiState.sendCryptoAmount.roundToEightDecimalPlaces()} ${viewModel.asset}",
                 style = Typography.h4.copy(fontWeight = FontWeight.W600),
                 color = MultimoneyTheme.colors.text,
                 textAlign = TextAlign.Center
@@ -69,7 +89,7 @@ fun SendCryptoAmountBottomSheetScreen(
                 )
                 Spacer(modifier = Modifier.width(8.dp))
                 Text(
-                    text = "$1.18", //TODO get from viewModel
+                    text = viewModel.uiState.sendDollarAmount,
                     style = Typography.body2.copy(fontWeight = FontWeight.W600),
                     color = Color.LightGray,
                     textAlign = TextAlign.Start
@@ -79,7 +99,7 @@ fun SendCryptoAmountBottomSheetScreen(
                 icon = R.drawable.ic_network_public,
                 iconColor = Color.White,
                 label = stringResource(id = R.string.crypto_send_amount_bottom_sheet_to_address),
-                text = "139cmexU1KpNMrgqBT7REqjf6mXQAvX5rQ", //TODO get from viewModel
+                text = viewModel.destinationAddress,
                 labelStyle = Typography.body2.copy(fontWeight = FontWeight.W600),
                 textStyle = Typography.body2.copy(fontWeight = FontWeight.Light),
                 labelColor = MultimoneyTheme.colors.text,
@@ -93,7 +113,7 @@ fun SendCryptoAmountBottomSheetScreen(
                 icon = R.drawable.ic_receipt,
                 iconColor = Color.White,
                 label = stringResource(id = R.string.crypto_send_amount_bottom_sheet_approximate_transaction_fee),
-                text = "0.0000059 BTC", //TODO get from viewModel
+                text = "${viewModel.uiState.transferCommission?.transferFee?.totalFee?.roundToEightDecimalPlaces()} ${viewModel.asset}",
                 labelStyle = Typography.body2.copy(fontWeight = FontWeight.W600),
                 textStyle = Typography.body2.copy(fontWeight = FontWeight.Light),
                 labelColor = MultimoneyTheme.colors.text,
@@ -109,7 +129,12 @@ fun SendCryptoAmountBottomSheetScreen(
                 modifier = Modifier
                     .height(48.dp)
                     .fillMaxWidth(),
-                onClick = { }, //TODO perform send action
+                onClick = {
+                    coroutineScope.launch {
+                        modalBottomSheetState.hide()
+                    }
+                    viewModel.onUIEvent(CryptoSendAmountViewModel.UIEvent.OnSendCryptoCurrency)
+                },
                 text = stringResource(id = R.string.crypto_send_amount_bottom_sheet_button_send),
                 buttonType = CustomButtonType.PrimaryPrimary
             )

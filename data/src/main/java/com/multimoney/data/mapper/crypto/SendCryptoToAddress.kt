26781b7515fc5a77cb0d2b3e3d1f04231c285062
Gustavo Müller <gmuller@applaudostudios.com>
2023-02-15 14:07:30 -0600

    :sparkles: Add info fields to mutation and response models

--- data/src/main/java/com/multimoney/data/mapper/crypto/SendCryptoToAddress.kt
+++ data/src/main/java/com/multimoney/data/mapper/crypto/SendCryptoToAddress.kt
@@ -20,7 +20,10 @@ private fun SendCryptoToAddressMutation.Result.mapToDomainModel() = SendCryptoTo
 )
 
 private fun SendCryptoToAddressMutation.TransferOrder.mapToDomainModel() = SendCryptoToAddressOrder(
-    result = result.mapToDomainModel()
+    result = result.mapToDomainModel(),
+    status = status,
+    message = message,
+    detail = detail
 )
 
 fun SendCryptoToAddressMutation.Data.mapToDomainModel() = SendCryptoToAddressData(

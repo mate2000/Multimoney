d40cc3f2979f315443335fa2d071b3db8cd87870
Jose Arenas <jarenas@applaudostudios.com>
2023-02-16 16:35:08 -0400

    adding support for GT in my accounts section

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/crypto/sell/selectaccount/SelectSmartAccountScreen.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/crypto/sell/selectaccount/SelectSmartAccountScreen.kt
@@ -27,6 +27,7 @@ import com.multimoney.multimoney.presentation.ui.crypto.sell.SellCryptoSharedVie
 import com.multimoney.multimoney.presentation.ui.home.profile.accounts.MyAccountsSkeleton
 import com.multimoney.multimoney.presentation.uielement.CustomInfoButton
 import com.multimoney.multimoney.presentation.util.NavEvent
+import com.multimoney.multimoney.presentation.util.capitalized
 import com.multimoney.multimoney.presentation.util.catalog.CurrencyType
 
 @Composable
@@ -93,7 +94,7 @@ fun SelectSmartAccountContent(
                         startIcon = getCurrencyLogo(account.currencyCode),
                         title = stringResource(
                             id = R.string.buy_crypto_multimoney_smart_account_template,
-                            account.currencyCode ?: ""
+                            account.currencyCode?.capitalized() ?: ""
                         ),
                         onClick = {
                             sharedViewModel.onUIEvent(

71ce09645e8944de30d836f32c0d22c84931b6cd
Luis Castillo <luisdacas17@hotmail.com>
2023-02-23 09:27:08 -0600

    chore: update send data to callback

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/PackageTrackerModule.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/PackageTrackerModule.kt
@@ -1,14 +1,15 @@
 package com.multimoney.multimoney.presentation.ui
 
 import com.facebook.react.bridge.*
+import com.google.gson.JsonParser
 
 class PackageTrackerModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
     @ReactMethod
-    fun callback(error: Boolean, response: String) {
-        if (!error) {
-            System.out.println("Show response card: " + response)
+    fun callback(error: String, response: String) {
+        if (error === "undefined") {
+            System.out.println("Show response card: " + JsonParser.parseString(response))
         } else {
-            System.out.println("Error: " + response)
+            System.out.println("Error: " + JsonParser.parseString(error))
         }
     }
 

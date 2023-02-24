b6b5659bcb6ddfecc0f4c88d678a4ee97ed67f47
mmilenkoff <mmilenkoff@applaudostudios.com>
2023-02-24 16:57:34 -0300

    :bug: fix Success to account titles

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/smart/transfer/smart/mycontactamount/MyContactsTransferSuccess.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/smart/transfer/smart/mycontactamount/MyContactsTransferSuccess.kt
@@ -57,7 +57,7 @@ fun MyContactsTransferSuccess(
             SmartPaymentInfoItem(
                 verticalAlignment = Alignment.CenterVertically,
                 icon = viewModel.destinyCurrency?.accountIcon,
-                title = stringResource(string.smart_payment_destiny_account_label),
+                title = viewModel.amountUIState.destinyAccountDisplay?.sheetTitle ?: "",
                 subtitle = if (Brand.CostaRica.id == viewModel.idBrand) {
                     getMaskedAccountIban(
                         viewModel.phoneAccount?.ibanNumber ?: ""
@@ -65,7 +65,7 @@ fun MyContactsTransferSuccess(
                 } else {
                     getMaskedAccount(
                         accountNumber = viewModel.phoneAccount?.accountNumber ?: "",
-                        prefix = Brand.ElSalvador.countryCode.uppercase()
+                        prefix = ""
                     )
                 }
             )

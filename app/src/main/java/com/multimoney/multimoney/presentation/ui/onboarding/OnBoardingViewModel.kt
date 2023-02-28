2c7cc4fdbcd1f71c056e01cc9248c726ca70fe3e
Antonio Guerra <anapoleon@applaudostudios.com>
2023-02-27 20:02:05 -0400

    🚧 REV-3852 Add splash and onboarding adjust events

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/onboarding/OnBoardingViewModel.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/onboarding/OnBoardingViewModel.kt
@@ -16,6 +16,7 @@ import com.multimoney.multimoney.presentation.ui.onboarding.OnBoardingViewModel.
 import com.multimoney.multimoney.presentation.ui.onboarding.OnBoardingViewModel.UIEvent.OnPress
 import com.multimoney.multimoney.presentation.util.ISO3_COSTA_RICA
 import com.multimoney.multimoney.presentation.util.ISO3_GUATEMALA
+import com.multimoney.multimoney.presentation.util.catalog.AdjustEventType
 import com.multimoney.multimoney.util.firebase.FireBaseEvents
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -23,7 +24,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class OnBoardingViewModel @Inject constructor(
-    private val dataStorePreferences: DataStorePreferences,
+    private val dataStorePreferences: DataStorePreferences
 ) : BaseViewModel(false) {
 
     // UIState
@@ -85,7 +86,7 @@ class OnBoardingViewModel @Inject constructor(
     private fun getStepContent(step: Int, context: Context): List<Int> = when (step) {
         STEP_ONE -> {
             provideFireBaseEventHelper.logEvent(FireBaseEvents.OnboardingOne)
-
+            registerAdjustEvent(AdjustEventType.ON_BOARDING_1, isLoggedIn = false)
             when (context.resources.configuration.locale.isO3Country) {
                 ISO3_COSTA_RICA -> listOf(
                     R.string.onboarding_costa_rica_step_one_title,
@@ -106,6 +107,7 @@ class OnBoardingViewModel @Inject constructor(
         }
         STEP_TWO -> {
             provideFireBaseEventHelper.logEvent(FireBaseEvents.OnboardingTwo)
+            registerAdjustEvent(AdjustEventType.ON_BOARDING_2, isLoggedIn = false)
             when (context.resources.configuration.locale.isO3Country) {
                 ISO3_COSTA_RICA -> listOf(
                     R.string.onboarding_costa_rica_step_two_title,
@@ -126,6 +128,7 @@ class OnBoardingViewModel @Inject constructor(
         }
         else -> {
             provideFireBaseEventHelper.logEvent(FireBaseEvents.OnboardingThree)
+            registerAdjustEvent(AdjustEventType.ON_BOARDING_3, isLoggedIn = false)
             when (context.resources.configuration.locale.isO3Country) {
                 ISO3_COSTA_RICA -> listOf(
                     R.string.onboarding_costa_rica_step_three_title,
@@ -147,7 +150,6 @@ class OnBoardingViewModel @Inject constructor(
     }
 
     private fun navigateToNextScreen(screen: String) {
-
         viewModelScope.launch {
             dataStorePreferences.isOnBoardingEnabled(false)
             popAndNavigateTo(
@@ -168,7 +170,7 @@ class OnBoardingViewModel @Inject constructor(
         val icon: Int = R.drawable.ic_onboarding_step_one,
 
         // Interactions
-        val isPressed: Boolean = false,
+        val isPressed: Boolean = false
     )
 
     fun onUIEvent(event: UIEvent, context: Context) {

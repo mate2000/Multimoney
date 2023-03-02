1cf44a8f661e201d26be1850db274132b3f845b7
mmilenkoff <mmilenkoff@applaudostudios.com>
2023-03-02 14:00:21 -0300

    :bug: fix transfer bottom sheet showing halfway

--- app/src/main/java/com/multimoney/multimoney/presentation/util/onfido/OnFidoHelper.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/util/onfido/OnFidoHelper.kt
@@ -14,6 +14,7 @@ import com.onfido.android.sdk.capture.ui.options.FlowStep
 import com.onfido.android.sdk.capture.ui.options.stepbuilder.DocumentCaptureStepBuilder
 import com.onfido.android.sdk.capture.utils.CountryCode
 import dagger.hilt.android.qualifiers.ApplicationContext
+import java.util.Locale
 import javax.inject.Inject
 
 class OnFidoHelper @Inject constructor(
@@ -31,7 +32,7 @@ class OnFidoHelper @Inject constructor(
         createFlowStepOptions(
             DocumentCaptureStepBuilder.forNationalIdentity().withCountry(getCountryCode(idBrand)).build()
         )
-    ).build()
+    ).withLocale(if (idBrand == 7) Locale.forLanguageTag("es-CR") else Locale.US).build()
 
     private fun getOnFidoConfigForSeveralDocuments(
         idBrand: Int?,

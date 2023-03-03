624aea187a0d0cf8d9c03a61c88cad75ae44b4a5
haroldcalayan-applaudo <hcalayan@applaudostudios.com>
2023-03-03 14:21:34 +0800

    :bug: REV-3954 Add image qrcode  on share receive address

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/crypto/receive/cryptoaddress/CryptoReceiveAddressScreen.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/crypto/receive/cryptoaddress/CryptoReceiveAddressScreen.kt
@@ -180,11 +180,18 @@ fun CryptoReceiveAddressContent(
                         contentDescription = "",
                         modifier = Modifier.clickable(onClick = {
                             if (!viewModel.uiState.isLoading) {
-                                viewModel.onUIEvent(
-                                    CryptoReceiveAddressViewModel.UIEvent.OnShareCryptoReceiveAddress(
-                                        viewModel.uiState.address
-                                    )
+                                val qrcodeBitmap = QRCodeGenerator.generateQrCode(
+                                    text = viewModel.uiState.address,
+                                    size = 720
                                 )
+                                if (qrcodeBitmap != null) {
+                                    viewModel.onUIEvent(
+                                        CryptoReceiveAddressViewModel.UIEvent.OnShareCryptoReceiveAddress(
+                                            viewModel.uiState.address,
+                                            qrcodeBitmap
+                                        )
+                                    )
+                                }
                             }
                         })
                     )
--- app/src/main/java/com/multimoney/multimoney/presentation/ui/crypto/receive/cryptoaddress/CryptoReceiveAddressViewModel.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/crypto/receive/cryptoaddress/CryptoReceiveAddressViewModel.kt
@@ -1,5 +1,6 @@
 package com.multimoney.multimoney.presentation.ui.crypto.receive.cryptoaddress
 
+import android.graphics.Bitmap
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -75,8 +76,8 @@ class CryptoReceiveAddressViewModel @Inject constructor(
         }
     }
 
-    private fun shareCryptoReceiveAddress(address: String) {
-        helper.shareTextPlain(text = address)
+    private fun shareCryptoReceiveAddress(address: String, bitmap: Bitmap) {
+        helper.shareTextWithImage(text = address, bitmap = bitmap)
     }
 
     fun onUIEvent(event: UIEvent) {
@@ -89,7 +90,7 @@ class CryptoReceiveAddressViewModel @Inject constructor(
                 event.cryptoNetwork
             )
             is UIEvent.OnGetCryptoReceiveAddress -> getCryptoReceiveAddress()
-            is UIEvent.OnShareCryptoReceiveAddress -> shareCryptoReceiveAddress(event.address)
+            is UIEvent.OnShareCryptoReceiveAddress -> shareCryptoReceiveAddress(event.address, event.bitmap)
             is UIEvent.OnSetOpenMaintenanceAction -> openMaintenanceAction = event.action
         }
     }
@@ -125,7 +126,8 @@ class CryptoReceiveAddressViewModel @Inject constructor(
         ) : UIEvent
         object OnGetCryptoReceiveAddress : UIEvent
         data class OnShareCryptoReceiveAddress(
-            val address: String
+            val address: String,
+            val bitmap: Bitmap
         ) : UIEvent
         data class OnSetOpenMaintenanceAction(val action: () -> Unit) : UIEvent
     }

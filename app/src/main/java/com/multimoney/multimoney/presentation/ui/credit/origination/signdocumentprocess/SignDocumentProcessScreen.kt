b54507d4b5d8c12cb97d483ea0c0b67456698801
Dsanchez1992 <103954910+Dsanchez1992@users.noreply.github.com>
2023-03-06 17:17:58 -0600

    :bug: REV-4166 fix bugs in CR  credit origination

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/credit/origination/signdocumentprocess/SignDocumentProcessScreen.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/credit/origination/signdocumentprocess/SignDocumentProcessScreen.kt
@@ -62,9 +62,11 @@ fun SignDocumentProcessScreen(
 
     when (viewModel.uiState.signDocumentProcessStep) {
         GENERATE_DOCUMENT_STEP.value -> {
-            DocumentGenerationScreen(onGetLinkAgain = {
-                viewModel.onUIEvent(OnCallGetLinkCreditContractSecondTime)
-            })
+            DocumentGenerationScreen(
+                idBran = viewModel.idBrand,
+                onGetLinkAgain = {
+                    viewModel.onUIEvent(OnCallGetLinkCreditContractSecondTime)
+                })
         }
         SIGN_DOCUMENTS_STEP.value -> {
             SignDocumentScreen(viewModel = viewModel)

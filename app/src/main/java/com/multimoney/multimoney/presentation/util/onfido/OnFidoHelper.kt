883a752dcf3c1893b1aeb9baba56ae78b847570f
mmilenkoff <mmilenkoff@applaudostudios.com>
2023-03-08 12:18:59 -0300

    :bug: force Locale to SV on default

--- app/src/main/java/com/multimoney/multimoney/presentation/util/onfido/OnFidoHelper.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/util/onfido/OnFidoHelper.kt
@@ -35,7 +35,6 @@ class OnFidoHelper @Inject constructor(
     ).withLocale(
         when (idBrand) {
             Brand.CostaRica.id -> Locale.forLanguageTag(CR_LANGUAGE_TAG)
-            Brand.ElSalvador.id -> Locale.forLanguageTag(SV_LANGUAGE_TAG)
             else -> Locale.forLanguageTag(SV_LANGUAGE_TAG)
         }
     ).build()

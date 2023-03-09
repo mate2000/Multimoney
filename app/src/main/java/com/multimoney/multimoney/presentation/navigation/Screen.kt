5a63fd5ff220c355cdc5f242235a76dadee19e1b
mmilenkoff <mmilenkoff@applaudostudios.com>
2023-03-09 19:35:48 -0300

    :bug: Fix nav errors

--- app/src/main/java/com/multimoney/multimoney/presentation/navigation/Screen.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/navigation/Screen.kt
@@ -22,7 +22,7 @@ import com.multimoney.multimoney.presentation.navigation.navgraph.IDENTIFICATION
 import com.multimoney.multimoney.presentation.navigation.navgraph.ID_CARD
 import com.multimoney.multimoney.presentation.navigation.navgraph.ID_CLIENT
 import com.multimoney.multimoney.presentation.navigation.navgraph.ID_CURRENCY
-import com.multimoney.multimoney.presentation.navigation.navgraph.ID_GLOBAL_REQUEST
+import com.multimoney.multimoney.presentation.navigation.navgraph.ID_GLOBAL_REQUEST_NAV
 import com.multimoney.multimoney.presentation.navigation.navgraph.ID_LOAN_CLIENT
 import com.multimoney.multimoney.presentation.navigation.navgraph.ID_USER_REQUEST
 import com.multimoney.multimoney.presentation.navigation.navgraph.INFO_USER
@@ -476,7 +476,7 @@ sealed class Screen(val route: String, val baseRoute: String = "") {
 
     // Smart
     object SmartScreen : Screen(
-        "smart_screen/{$USER}/{$ID_BRAND}/{$PK_USER}/{$IDENTIFICATION}/{$EMAIL}/{$CREDIT_STEP}/{$FIRST_NAME}/{$LAST_NAME}/{$COMING_FROM_CRYPTO}/{$ID_GLOBAL_REQUEST}/{$ID_USER_REQUEST}/{$EVICERTIA_STATUS}/{$WORK_FLOW}",
+        "smart_screen/{$USER}/{$ID_BRAND}/{$PK_USER}/{$IDENTIFICATION}/{$EMAIL}/{$CREDIT_STEP}/{$FIRST_NAME}/{$LAST_NAME}/{$COMING_FROM_CRYPTO}/{$ID_GLOBAL_REQUEST_NAV}/{$ID_USER_REQUEST}/{$EVICERTIA_STATUS}/{$WORK_FLOW}",
         "smart_screen"
     )
 

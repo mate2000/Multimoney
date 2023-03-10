27c6f1a3c9fbdf276c44731f4af3092e0cfa1b44
rescalon34 <roberto.escalon34@gmail.com>
2023-03-09 21:49:56 -0600

    :bug: REV- 4498 - fixing navigation with onfido

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/smart/SmartScreen.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/smart/SmartScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.multimoney.data.util.catalog.Brand
 import com.multimoney.data.util.catalog.SmartSteps
+import com.multimoney.data.util.catalog.SmartWorkflow
 import com.multimoney.multimoney.R
 import com.multimoney.multimoney.presentation.theme.MultimoneyTheme
 import com.multimoney.multimoney.presentation.ui.smart.SmartViewModel.UIEvent.OnBackClick
@@ -72,49 +73,52 @@ fun SmartScreen(
             .fillMaxSize()
             .background(MultimoneyTheme.colors.background)
     ) {
-        Column {
-            TopNavBar(
-                isLeftButtonVisible = viewModel.uiState.currentStep <= viewModel.getTotalStepperCounter(),
-                isRightButtonVisible = viewModel.uiState.isCloseVisible,
-                onLeftButtonClick = { viewModel.onUIEvent(OnBackClick(focusManager)) },
-                onRightButtonClick = { viewModel.onUIEvent(OnCloseClick(focusManager)) }
+
+        if (viewModel.workflow != SmartWorkflow.SMART_ONFIDO_PROCESS.workflow) {
+            Column {
+                TopNavBar(
+                    isLeftButtonVisible = viewModel.uiState.currentStep <= viewModel.getTotalStepperCounter(),
+                    isRightButtonVisible = viewModel.uiState.isCloseVisible,
+                    onLeftButtonClick = { viewModel.onUIEvent(OnBackClick(focusManager)) },
+                    onRightButtonClick = { viewModel.onUIEvent(OnCloseClick(focusManager)) }
+                )
+                if (viewModel.uiState.currentStep <= viewModel.getTotalStepperCounter()) {
+                    StepProgressBar(
+                        steps = viewModel.getTotalStepperCounter(),
+                        currentStep = viewModel.uiState.currentStep,
+                        modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+                    )
+                }
+            }
+            Column(
+                modifier = Modifier.weight(0.1f),
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                // Each country has it's own steps on the smart origination flow
+                if (viewModel.idBrandAsInt == Brand.ElSalvador.id) {
+                    GetSvStepContent(
+                        step = viewModel.uiState.currentStep,
+                        viewModel = viewModel
+                    )
+                } else {
+                    GetCrStepContent(
+                        step = viewModel.uiState.currentStep,
+                        viewModel = viewModel
+                    )
+                }
+            }
+            CustomButton(
+                onClick = { viewModel.onUIEvent(OnContinueClick(focusManager)) },
+                text = stringResource(id = viewModel.uiState.buttonTextRes),
+                modifier = Modifier
+                    .padding(start = 16.dp, end = 16.dp, bottom = 32.dp, top = 16.dp)
+                    .fillMaxWidth()
+                    .height(48.dp),
+                buttonType = PrimaryPrimary,
+                enable = viewModel.uiState.isContinueEnabled,
+                visible = viewModel.uiState.isContinueVisible
             )
-            if (viewModel.uiState.currentStep <= viewModel.getTotalStepperCounter()) {
-                StepProgressBar(
-                    steps = viewModel.getTotalStepperCounter(),
-                    currentStep = viewModel.uiState.currentStep,
-                    modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
-                )
-            }
         }
-        Column(
-            modifier = Modifier.weight(0.1f),
-            verticalArrangement = Arrangement.SpaceBetween
-        ) {
-            // Each country has it's own steps on the smart origination flow
-            if (viewModel.idBrandAsInt == Brand.ElSalvador.id) {
-                GetSvStepContent(
-                    step = viewModel.uiState.currentStep,
-                    viewModel = viewModel
-                )
-            } else {
-                GetCrStepContent(
-                    step = viewModel.uiState.currentStep,
-                    viewModel = viewModel
-                )
-            }
-        }
-        CustomButton(
-            onClick = { viewModel.onUIEvent(OnContinueClick(focusManager)) },
-            text = stringResource(id = viewModel.uiState.buttonTextRes),
-            modifier = Modifier
-                .padding(start = 16.dp, end = 16.dp, bottom = 32.dp, top = 16.dp)
-                .fillMaxWidth()
-                .height(48.dp),
-            buttonType = PrimaryPrimary,
-            enable = viewModel.uiState.isContinueEnabled,
-            visible = viewModel.uiState.isContinueVisible
-        )
     }
 
     LoadingIndicator(viewModel.uiState.isLoading)

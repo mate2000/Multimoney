892b7b6a25c972895f08ff32436367868a0f54a3
jtorres-applaudo <jatorres@applaudostudios.com>
2023-03-17 14:58:52 -0600

    Update onSuccess flow for user validation

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/login/signup/email/SignUpEmailScreen.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/login/signup/email/SignUpEmailScreen.kt
@@ -93,15 +93,19 @@ fun SignUpEmailScreen(
                         false
                     )
                 )
-                if (userData?.status == SignUpEmailViewModel.ANOTHER_DEVICE_ALREADY_REGISTERED) {
-                    viewModel.onUIEvent(
-                        OnShowAnotherDeviceAlreadyRegisteredDialog {
-                            viewModel.onSuccessValidation(context, sharedViewModel, userData)
-                        }
-                    )
-                    sharedViewModel.logEvents(null, AdjustEventType.SECURITY_SIGN_UP_CHANGE_DEVICE_9001)
+                if(userData?.isNewUser == false) {
+                    sharedViewModel.onUIEvent(SignUpViewModel.UIEvent.OnCheckIfEmailExists(userData))
                 } else {
-                    viewModel.onSuccessValidation(context, sharedViewModel, userData)
+                    if (userData?.status == SignUpEmailViewModel.ANOTHER_DEVICE_ALREADY_REGISTERED) {
+                        viewModel.onUIEvent(
+                            OnShowAnotherDeviceAlreadyRegisteredDialog {
+                                viewModel.onSuccessValidation(context, sharedViewModel, userData)
+                            }
+                        )
+                        sharedViewModel.logEvents(null, AdjustEventType.SECURITY_SIGN_UP_CHANGE_DEVICE_9001)
+                    } else {
+                        viewModel.onSuccessValidation(context, sharedViewModel, userData)
+                    }
                 }
             }.onMessage {
                 viewModel.onUIEvent(

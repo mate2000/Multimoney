ddb886e1920a335e43943d571b1107412142da01
jtorres-applaudo <jatorres@applaudostudios.com>
2023-03-20 12:30:34 -0600

    Create ui event to update previous email

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/login/signup/email/SignUpEmailViewModel.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/login/signup/email/SignUpEmailViewModel.kt
@@ -104,7 +104,6 @@ class SignUpEmailViewModel @Inject constructor(
         openSignUpSplashComeBack: () -> Unit,
         onOpenDialog: (DialogParameters) -> Unit
     ) {
-        previousUserEmail = userData?.email ?: ""
         onUseDataValueChange()
         if (userData?.status == VALID_EMAIL || userData?.status == ANOTHER_DEVICE_ALREADY_REGISTERED) {
             val step = SignUpStep.Search.getIdByName(userData.currentStep)
@@ -128,6 +127,10 @@ class SignUpEmailViewModel @Inject constructor(
                 )
             )
         }
+    }
+
+    private fun setPreviousEmail() {
+        previousUserEmail = uiState.userEmail
     }
 
     private fun onHandleUserState(
@@ -220,6 +223,7 @@ class SignUpEmailViewModel @Inject constructor(
             is OnValidateUserEmail -> isUserEmailValid()
             is OnUserEmailValueChange -> onUserEmailValueChange(event.value)
             is OnShowAnotherDeviceAlreadyRegisteredDialog -> onShowAnotherDeviceAlreadyRegisteredDialog(event.onPositiveClick)
+            is OnSetPreviousEmail -> setPreviousEmail()
         }
     }
 
@@ -244,6 +248,7 @@ class SignUpEmailViewModel @Inject constructor(
         object OnValidateForm : UIEvent()
         object OnValidateUserEmail : UIEvent()
         data class OnShowAnotherDeviceAlreadyRegisteredDialog(val onPositiveClick: () -> Unit) : UIEvent()
+        object OnSetPreviousEmail : UIEvent()
     }
 
     sealed class BaseEvent {

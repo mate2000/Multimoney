06dc11322224566da27cbdd03119fde7e94653d9
mmilenkoff <mmilenkoff@applaudostudios.com>
2023-03-29 14:09:21 -0300

    :bug: fix forgot password otp field

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/login/forgotpassword/process/ProcessForgotPasswordScreen.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/login/forgotpassword/process/ProcessForgotPasswordScreen.kt
@@ -176,7 +176,7 @@ fun ProcessForgotPasswordContent(
                 placeHolder = stringResource(id = string.process_forgot_password_otp_placeholder),
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 24.dp, start = 16.dp, end = 16.dp),
+                    .padding(top = 24.dp),
                 isRequired = true,
                 isRequiredMessage = stringResource(id = string.process_forgot_password_otp_required)
             )

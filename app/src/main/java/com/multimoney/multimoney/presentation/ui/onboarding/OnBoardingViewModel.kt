0dbca4ce4a406992ed1fdb313de8f90a67c0a943
jtorres-applaudo <jatorres@applaudostudios.com>
2023-03-30 13:10:33 -0600

    Remove unused import

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/onboarding/OnBoardingViewModel.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/onboarding/OnBoardingViewModel.kt
@@ -1,7 +1,6 @@
 package com.multimoney.multimoney.presentation.ui.onboarding
 
 import android.content.Context
-import android.util.Log
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf

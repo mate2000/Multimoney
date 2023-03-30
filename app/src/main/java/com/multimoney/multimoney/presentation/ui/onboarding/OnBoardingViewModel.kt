1c8cb8affdc7dc36612af8b23c26cce696c9068d
jtorres-applaudo <jatorres@applaudostudios.com>
2023-03-30 12:33:40 -0600

    Create method to get sim code

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/onboarding/OnBoardingViewModel.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/onboarding/OnBoardingViewModel.kt
@@ -1,6 +1,7 @@
 package com.multimoney.multimoney.presentation.ui.onboarding
 
 import android.content.Context
+import android.util.Log
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,15 +16,15 @@ import com.multimoney.multimoney.presentation.ui.onboarding.OnBoardingViewModel.
 import com.multimoney.multimoney.presentation.ui.onboarding.OnBoardingViewModel.UIEvent.OnInitializeResources
 import com.multimoney.multimoney.presentation.ui.onboarding.OnBoardingViewModel.UIEvent.OnNavigateToNextScreen
 import com.multimoney.multimoney.presentation.ui.onboarding.OnBoardingViewModel.UIEvent.OnPress
-import com.multimoney.multimoney.presentation.util.FIRST_INDEX
-import com.multimoney.multimoney.presentation.util.ISO3_COSTA_RICA
-import com.multimoney.multimoney.presentation.util.ISO3_GUATEMALA
+import com.multimoney.multimoney.presentation.util.SIM_CODE_COSTA_RICA
+import com.multimoney.multimoney.presentation.util.SIM_CODE_GUATEMALA
 import com.multimoney.multimoney.presentation.util.catalog.AdjustEventType
+import com.multimoney.multimoney.presentation.util.getUserCountry
 import com.multimoney.multimoney.util.firebase.FireBaseEvents
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @HiltViewModel
 class OnBoardingViewModel @Inject constructor(
@@ -95,17 +96,20 @@ class OnBoardingViewModel @Inject constructor(
         }
     }
 
+    private fun getCountryCode(context: Context) = if(context.getUserCountry() == "") DEFAULT_REGION
+        else context.getUserCountry()
+
     private fun getStepContent(step: Int, context: Context): List<Int> = when (step) {
         STEP_ONE -> {
             provideFireBaseEventHelper.logEvent(FireBaseEvents.OnboardingOne)
             registerAdjustEvent(AdjustEventType.ON_BOARDING_1_1002, isLoggedIn = false)
-            when (context.resources.configuration.locales.get(FIRST_INDEX).isO3Country) {
-                ISO3_COSTA_RICA -> listOf(
+            when (getCountryCode(context)) {
+                SIM_CODE_COSTA_RICA -> listOf(
                     R.string.onboarding_costa_rica_step_one_title,
                     R.string.onboarding_costa_rica_step_one_sub_title,
                     R.drawable.ic_onboarding_step_one
                 )
-                ISO3_GUATEMALA -> listOf(
+                SIM_CODE_GUATEMALA -> listOf(
                     R.string.onboarding_guatemala_step_one_title,
                     R.string.onboarding_guatemala_step_one_sub_title,
                     R.drawable.ic_onboarding_step_one
@@ -120,13 +124,13 @@ class OnBoardingViewModel @Inject constructor(
         STEP_TWO -> {
             provideFireBaseEventHelper.logEvent(FireBaseEvents.OnboardingTwo)
             registerAdjustEvent(AdjustEventType.ON_BOARDING_2_1003, isLoggedIn = false)
-            when (context.resources.configuration.locale.isO3Country) {
-                ISO3_COSTA_RICA -> listOf(
+            when (getCountryCode(context)) {
+                SIM_CODE_COSTA_RICA -> listOf(
                     R.string.onboarding_costa_rica_step_two_title,
                     R.string.onboarding_costa_rica_step_two_sub_title,
                     R.drawable.ic_onboarding_step_two
                 )
-                ISO3_GUATEMALA -> listOf(
+                SIM_CODE_GUATEMALA -> listOf(
                     R.string.onboarding_guatemala_step_two_title,
                     R.string.onboarding_guatemala_step_two_sub_title,
                     R.drawable.ic_onboarding_step_two
@@ -141,13 +145,13 @@ class OnBoardingViewModel @Inject constructor(
         else -> {
             provideFireBaseEventHelper.logEvent(FireBaseEvents.OnboardingThree)
             registerAdjustEvent(AdjustEventType.ON_BOARDING_3_1004, isLoggedIn = false)
-            when (context.resources.configuration.locale.isO3Country) {
-                ISO3_COSTA_RICA -> listOf(
+            when (getCountryCode(context)) {
+                SIM_CODE_COSTA_RICA -> listOf(
                     R.string.onboarding_costa_rica_step_three_title,
                     R.string.onboarding_costa_rica_step_three_sub_title,
                     R.drawable.ic_onboarding_step_three
                 )
-                ISO3_GUATEMALA -> listOf(
+                SIM_CODE_GUATEMALA -> listOf(
                     R.string.onboarding_guatemala_step_three_title,
                     R.string.onboarding_guatemala_step_three_sub_title,
                     R.drawable.ic_onboarding_step_three
@@ -215,5 +219,6 @@ class OnBoardingViewModel @Inject constructor(
         const val STEP_ICON = 2
         const val TOTAL_PRESS_TIME = 300
         const val QUARTER = 4
+        const val DEFAULT_REGION = "sv"
     }
 }

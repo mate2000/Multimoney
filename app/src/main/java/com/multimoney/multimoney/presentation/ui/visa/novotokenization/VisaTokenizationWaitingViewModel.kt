73c8afe8edef1f15e5d2b079fd7f169b511b1971
Dsanchez1992 <103954910+Dsanchez1992@users.noreply.github.com>
2023-04-19 18:32:21 -0600

    :fire: REV-5242 Remove unused import

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/visa/novotokenization/VisaTokenizationWaitingViewModel.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/visa/novotokenization/VisaTokenizationWaitingViewModel.kt
@@ -1,7 +1,6 @@
 package com.multimoney.multimoney.presentation.ui.visa.novotokenization
 
 import android.content.Context
-import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue

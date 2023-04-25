cb78de5c01e905afadba87482a57aaf0ddc1cd67
Luis Enrique Ramirez <leramirez@applaudostudios.com>
2023-04-25 17:29:56 -0400

    removing unnecessary scroll on BuyCurrencyScreen

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/crypto/purchase/buycurrency/BuyCurrencyScreen.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/crypto/purchase/buycurrency/BuyCurrencyScreen.kt
@@ -9,9 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.ModalBottomSheetValue
@@ -326,7 +324,7 @@ fun BuyCurrencyScreenContent(
                         start.linkTo(parent.start)
                         end.linkTo(parent.end)
                         bottom.linkTo(button.top)
-                    }.verticalScroll(rememberScrollState())
+                    }
                 ) {
                     CounterSection(
                         counterTextResourceId = R.string.crypto_purchase_flow_price_expires_in,

41420936f946f8cd29aa5b9d45978c03e546cbbf
dhguevarah <dguevara@applaudostudios.com>
2023-05-03 10:04:52 -0600

    :bug: REV-5467 Open add account screen from payment schedule

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/credit/payment/schedule/account/PaymentScheduleAccountViewModel.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/credit/payment/schedule/account/PaymentScheduleAccountViewModel.kt
@@ -4,33 +4,43 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewModelScope
+import com.multimoney.data.util.DataStorePreferences
 import com.multimoney.domain.interaction.credit.QueryGetClientBankAccountUseCase
 import com.multimoney.domain.model.credit.ClientBankAccount
+import com.multimoney.domain.model.metrics.BaseEventDataDto
 import com.multimoney.domain.model.util.onFailure
 import com.multimoney.domain.model.util.onLoading
 import com.multimoney.domain.model.util.onSuccess
 import com.multimoney.multimoney.presentation.base.BaseViewModel
 import com.multimoney.multimoney.presentation.navigation.ID_BRAND
 import com.multimoney.multimoney.presentation.navigation.Screen
+import com.multimoney.multimoney.presentation.navigation.navgraph.IDENTIFICATION
 import com.multimoney.multimoney.presentation.navigation.navgraph.ID_CLIENT
 import com.multimoney.multimoney.presentation.navigation.navgraph.ID_LOAN_CLIENT
 import com.multimoney.multimoney.presentation.navigation.navgraph.PAYMENT_DATE
 import com.multimoney.multimoney.presentation.navigation.navgraph.PREVIOUS_SCREEN
 import com.multimoney.multimoney.presentation.navigation.navgraph.USER
 import com.multimoney.multimoney.presentation.navigation.util.encodeData
+import com.multimoney.multimoney.presentation.ui.credit.payment.schedule.account.PaymentScheduleAccountViewModel.UIEvent.OnAddAccountClick
 import com.multimoney.multimoney.presentation.ui.credit.payment.schedule.account.PaymentScheduleAccountViewModel.UIEvent.OnCallQueryGetClientBankAccount
 import com.multimoney.multimoney.presentation.ui.credit.payment.schedule.account.PaymentScheduleAccountViewModel.UIEvent.OnClientBankAccountSelected
 import com.multimoney.multimoney.presentation.ui.credit.payment.schedule.account.PaymentScheduleAccountViewModel.UIEvent.OnNavigateBack
 import com.multimoney.multimoney.presentation.ui.home.HomeState
+import com.multimoney.multimoney.presentation.util.catalog.AdjustEventType
 import com.multimoney.multimoney.presentation.util.catalog.DialogParameters
+import com.multimoney.multimoney.presentation.util.toJson
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
 class PaymentScheduleAccountViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    private val queryGetClientBankAccountUseCase: QueryGetClientBankAccountUseCase
+    private val queryGetClientBankAccountUseCase: QueryGetClientBankAccountUseCase,
+    private val dataStorePreferences: DataStorePreferences
 ) : BaseViewModel(true) {
 
     // uiState
@@ -43,6 +53,7 @@ class PaymentScheduleAccountViewModel @Inject constructor(
     private var idClient: Int = 0
     private var idLoanClient: Int = 0
     private var paymentDate: String? = null
+    private var identification: String? = null
     private var previousScreen = ""
 
     init {
@@ -51,6 +62,7 @@ class PaymentScheduleAccountViewModel @Inject constructor(
         idClient = savedStateHandle[ID_CLIENT] ?: 0
         idLoanClient = savedStateHandle[ID_LOAN_CLIENT] ?: 0
         paymentDate = savedStateHandle[PAYMENT_DATE]
+        identification = savedStateHandle[IDENTIFICATION] ?: ""
         previousScreen = savedStateHandle[PREVIOUS_SCREEN] ?: ""
     }
 
@@ -83,12 +95,46 @@ class PaymentScheduleAccountViewModel @Inject constructor(
         }
     }
 
+    private fun getAddAccountEvent(baseAdjustEvent: BaseEventDataDto) =
+        suspend {
+            if (dataStorePreferences.isAdjustAddAccountEventRegister().first()) {
+                registerAdjustEvent(
+                    AdjustEventType.SETTINGS_FIRST_ADD_ACCOUNT_8003,
+                    applyAdjust = false,
+                    data = baseAdjustEvent.toJson()
+                )
+                dataStorePreferences.isAdjustAddAccountEventRegister(false)
+            }
+        }
+
+    private fun getAdjustEvent(adjustEventType: AdjustEventType): suspend () -> Unit {
+        val baseAdjustEvent = BaseEventDataDto(
+            user = user,
+            idBrand = idBrand,
+            idClient = idClient,
+            idLoanClient = idLoanClient,
+            identification = identification
+        )
+        return when (adjustEventType) {
+            AdjustEventType.SETTINGS_FIRST_ADD_ACCOUNT_8003 -> {
+                getAddAccountEvent(baseAdjustEvent)
+            }
+            else -> suspend {}
+        }
+    }
+
+    fun logEvents(adjustEventType: AdjustEventType) {
+        viewModelScope.launch {
+            getAdjustEvent(adjustEventType).invoke()
+        }
+    }
+
     private fun onClientBankAccountSelected(clientBankAccount: ClientBankAccount?) = popAndNavigateTo(
         route = "${Screen.PaymentScheduleScreen.baseRoute}/$user/$idBrand/$idClient/$idLoanClient/${
-            encodeData(
-                clientBankAccount
-            )
-        }/$paymentDate/${true}/$previousScreen/${false}",
+        encodeData(
+            clientBankAccount
+        )
+        }/$paymentDate/${true}/$previousScreen/${false}/$identification",
         popTo = Screen.PaymentScheduleAccountScreen.baseRoute
     )
 
@@ -96,6 +142,13 @@ class PaymentScheduleAccountViewModel @Inject constructor(
 
     private fun onCloseClick() =
         navigateBack(popTo = Screen.PaymentScheduleScreen.route, isRestart = false, homeState = HomeState.COLLAPSED)
+
+    private fun onAddAccountClick() {
+        logEvents(AdjustEventType.SETTINGS_FIRST_ADD_ACCOUNT_8003)
+        navigateTo(
+            route = "${Screen.AddIbanAccountScreen.baseRoute}/$user/$idBrand/$identification/${Screen.PaymentScheduleAccountScreen.baseRoute}/$idClient/$idLoanClient"
+        )
+    }
 
     data class UIState(
         // Interactions
@@ -110,6 +163,7 @@ class PaymentScheduleAccountViewModel @Inject constructor(
             is UIEvent.OnCloseClick -> onCloseClick()
             is OnCallQueryGetClientBankAccount -> onCallQueryGetClientBankAccountUseCase()
             is OnClientBankAccountSelected -> onClientBankAccountSelected(uiEvent.clientBankAccount)
+            is OnAddAccountClick -> onAddAccountClick()
         }
     }
 
@@ -118,5 +172,6 @@ class PaymentScheduleAccountViewModel @Inject constructor(
         class OnClientBankAccountSelected(val clientBankAccount: ClientBankAccount?) : UIEvent()
         object OnNavigateBack : UIEvent()
         object OnCloseClick : UIEvent()
+        object OnAddAccountClick : UIEvent()
     }
 }

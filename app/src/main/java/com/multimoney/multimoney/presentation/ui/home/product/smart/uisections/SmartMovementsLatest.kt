1cb81b13b1e53a12d9796697689a79e3f9987702
mmilenkoff <mmilenkoff@applaudostudios.com>
2023-05-04 13:57:47 -0300

    :art: fix padding

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/home/product/smart/uisections/SmartMovementsLatest.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/home/product/smart/uisections/SmartMovementsLatest.kt
@@ -46,7 +46,7 @@ fun SmartMovementsLatest(viewModel: ProductViewModel, index: Int) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 16.dp, bottom = 8.dp),
+                .padding(top = 8.dp, bottom = 16.dp),
             horizontalArrangement = SpaceBetween
         ) {
             Text(

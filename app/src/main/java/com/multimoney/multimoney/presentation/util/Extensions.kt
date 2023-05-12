2b3eebbc7a07fa8ca8433ff2afe117809699db9e
lgonzalez98 <lgonzalez@applaudostudios.com>
2023-05-12 16:02:45 -0600

    :recycle: REV-5688 - Add separate phone number function to add space in phone number

--- app/src/main/java/com/multimoney/multimoney/presentation/util/Extensions.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/util/Extensions.kt
@@ -475,12 +475,26 @@ fun String.getAddCardErrorFromValue(): AddVisaCardErrors =
  * @param phoneWithCode The phone number with the country code.
  * @param phoneWithoutCode The phone number without the country code.
  */
-fun formatPhoneNumber(phoneWithCode: String?, phoneWithoutCode: String?): String  {
+fun formatPhoneNumber(phoneWithCode: String?, phoneWithoutCode: String?): String {
     val stringBuilder = StringBuilder()
     val areaCode = phoneWithCode?.replace(phoneWithoutCode ?: "", "")
-    val spacedNumber = stringBuilder.append(phoneWithoutCode?.takeLast(PHONE_NUMBER_LENGTH))
-        .insert(PHONE_NUMBER_LENGTH / TWO, SPACE).toString()
+    val spacedNumber = separatePhoneNumber(phoneWithoutCode.orEmpty())
     return stringBuilder.clear().append(areaCode).append(SPACE).append(spacedNumber).toString()
+}
+
+fun separatePhoneNumber(phone: String): String {
+    if (phone.isEmpty()) return phone
+    val stringBuilder = StringBuilder()
+    return if (phone.length > PHONE_NUMBER_LENGTH) {
+        val number = phone.takeLast(PHONE_NUMBER_LENGTH)
+        val areaCode = phone.take(phone.length - number.length)
+        val spacedNumber = stringBuilder.append(number)
+            .insert(PHONE_NUMBER_LENGTH / TWO, SPACE).toString()
+        stringBuilder.clear().append(areaCode).append(SPACE).append(spacedNumber).toString()
+    } else {
+        stringBuilder.append(phone.takeLast(PHONE_NUMBER_LENGTH))
+            .insert(PHONE_NUMBER_LENGTH / TWO, SPACE).toString()
+    }
 }
 
 /**

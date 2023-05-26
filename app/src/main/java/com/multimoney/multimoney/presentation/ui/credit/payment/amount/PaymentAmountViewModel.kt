568f362f5d86e8988f1fba99af09ff6b69fd7cab
Dsanchez1992 <103954910+Dsanchez1992@users.noreply.github.com>
2023-05-26 08:39:40 -0600

    :bug: REV-5971 Fix bug that allows type many zeros

--- app/src/main/java/com/multimoney/multimoney/presentation/ui/credit/payment/amount/PaymentAmountViewModel.kt
+++ app/src/main/java/com/multimoney/multimoney/presentation/ui/credit/payment/amount/PaymentAmountViewModel.kt
@@ -55,6 +55,7 @@ import com.multimoney.multimoney.presentation.util.getCurrencyFromId
 import com.multimoney.multimoney.presentation.util.isValidAmount
 import com.multimoney.multimoney.presentation.util.stringToDoubleFormat
 import com.multimoney.multimoney.presentation.util.toJson
+import com.multimoney.multimoney.presentation.util.validateDecimalAmount
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.first
@@ -207,7 +208,7 @@ class PaymentAmountViewModel @Inject constructor(
     }
 
     private fun onAmountValueChange(value: String) {
-        if (value.isValidAmount()) {
+        if (value.validateDecimalAmount()) {
             val convertedValue = value.toDoubleOrNull()?.roundToInt()
             uiState = uiState.copy(
                 currentAmountValueString = value,
